06ee30e11b42017152cbb007a7906e644765c5bc
khnemu11 <37679352+khnemu11@users.noreply.github.com>
2022-08-17 23:59:29 +0900

    [level 2] Title: 문자열 압축, Time: 0.09 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -5,32 +5,31 @@ import java.util.List;
 class Solution {
     public int solution(String s) {
       int answer = s.length();
-		
+		//최대한 나눌 수 있는건 절반 크기이므로 1~절반까지 수행
 		for(int i=1;i<=s.length()/2;i++) {
 			StringBuilder sb = new StringBuilder();
 			int part = i;
 			int duplicateCount = 1;
+            //n 개 단위로 분할할 수 있을때 까지 분할
 			for(int j=0;j+part*2<=s.length();j+=part) {
-			//	System.out.println("start");
-			//	System.out.println(s.substring(j,j+part) + " " + s.substring(j+part,j+part*2));
+                //이번 문자열과 다음 문자열이 겹치면 겹침 변수 1 더함
 				if(s.substring(j,j+part).equals(s.substring(j+part,j+part*2))) {
-				//	System.out.println(s.substring(j,j+part) +" is same");
 					duplicateCount++;
 				}
+                //이번 문자열과 다음 문자열이 안겹치면 이번 문자열을 문자열에 추가
 				else {
-				//	System.out.println(s.substring(j,j+part) +" is different");
+                    //이전에 겹치던 문자열의 개수만큼 숫자를 추가함
 					if(duplicateCount>1) {
 						sb.append(duplicateCount);
 						sb.append(s.substring(j, j+part));
 						duplicateCount = 1;
 					}
 					else {
-					//	System.out.println("append "+s.substring(j,j+part));
 						sb.append(s.substring(j, j+part));
 					}
 				}
 			}
-			//System.out.println("loop out");
+            //비교했던 마지막 문자열과 나머지 문자열을 추가하는 조건문
 			if(duplicateCount!=1) {
 				sb.append(duplicateCount);
 				sb.append(s.substring(s.length()-part-s.length()%part, s.length()));
@@ -38,14 +37,10 @@ class Solution {
 			else {
 				sb.append(s.substring(s.length()-part-s.length()%part, s.length()));
 			}
-			//System.out.println("result : " + sb);
-			//System.out.println("==============");
 			if(sb.length()<answer) {
 				answer= sb.length();
 			}
 		}
-		
-	//	System.out.println("answer : "+answer);
         return answer;
     }
 }
