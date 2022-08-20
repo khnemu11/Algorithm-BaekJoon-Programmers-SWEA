5b18fbca6caecc00260a640677f30da54d9b2bfe
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2022-08-20 20:35:07 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  int input[3];
+  
+  scanf("%d",&input[0]);
+
+  for(int i=0;i<input[0];i++){
+    scanf("%d %d",&input[1],&input[2]);
+    printf("Case #%d: %d + %d = %d\n",i+1,input[1],input[2],input[1]+input[2]);
+  }
+}
