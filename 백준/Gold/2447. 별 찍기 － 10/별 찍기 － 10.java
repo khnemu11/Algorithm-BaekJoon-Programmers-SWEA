28ac44e40c7ebc387adb84281fce80b8aecc12fe
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2022-08-26 02:53:43 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 396 ms, Memory: 26832 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,41 +1,52 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String[][] arr;
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(bf.readLine());
-        arr = new String[N][N];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
-                arr[i][j] = " ";
-            }
-        }
-        star(arr, 0, 0, N);
 
-        for (String[] strings : arr) {
-            for (String string : strings) {
-                bw.write(string + "");
-            }
-            bw.write("\n");
-        }
-        bw.flush();
-        bw.close();
-    }
-
-    public static void star(String[][] arr, int x, int y, int N) {
-        if (N == 1) {
-            arr[x][y] = "*";
-            return;
-        }
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (!(i == 1 && j == 1)) {
-                    star(arr, x + i * (N / 3), y + j * (N / 3), N / 3);
-                }
-            }
-        }
-    }
+		public static void main(String []args) throws Exception{	
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+			int num =Integer.valueOf(br.readLine());
+			char[][] picture = new char[num][num];
+			
+			for(int i=0;i<num;i++) {
+				for(int j=0;j<num;j++) {
+					picture[i][j]=' ';
+				}
+			}
+			
+			draw(0,0,num,picture);
+			for(int i=0;i<num;i++) {
+				for(int j=0;j<num;j++) {
+					bw.append(picture[i][j]);
+				}
+				bw.newLine();
+			}
+			bw.flush();
+			bw.close();
+			br.close();
+		}
+		public static void draw(int x,int y,int size,char[][]picture) {
+	    	if(size==1) {
+	    		picture[x][y]='*';
+	    		return;
+	    	}
+	    	else {
+	    		size=size/3;
+	    		
+	    		draw(x,y,size,picture);
+	    		draw(x+size,y,size,picture);
+	    		draw(x+size*2,y,size,picture);
+	    		
+	    		draw(x,y+size,size,picture);
+	    		draw(x+size*2,y+size,size,picture);
+	    		
+	    		draw(x,y+size*2,size,picture);
+	    		draw(x+size,y+size*2,size,picture);
+	    		draw(x+size*2,y+size*2,size,picture);
+	    	}
+	    }
 }
