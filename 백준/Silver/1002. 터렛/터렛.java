3aa07dc551607a0ae43535341de8108d861643ce
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2022-08-28 20:40:51 +0900

    [Silver III] Title: 터렛, Time: 128 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.java
+++ 백준/Silver/1002. 터렛/터렛.java
@@ -1,10 +1,13 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 		public static void main(String []args) throws Exception{	
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 			int N = Integer.valueOf(br.readLine());
 			
 			for(int i=0;i<N;i++) {
@@ -16,25 +19,31 @@ public class Main {
 				int baekY = Integer.valueOf(st.nextToken());
 				int baekR = Integer.valueOf(st.nextToken());
 				double distance = Math.sqrt(Math.pow(baekX-joX,2) + Math.pow(baekY-joY,2));
-				
 				if(joX==baekX && joY==baekY && baekR==joR){
-					System.out.println(-1);
+					bw.write("-1");
+					bw.newLine();
 				}
 				else if(joR+baekR< distance){
-					System.out.println(0);
+					bw.write("0");
+					bw.newLine();
 				}
 				else if(joR+baekR==distance|| Math.abs(joR-baekR)==distance){
-					System.out.println(1);
+					bw.write("1");
+					bw.newLine();
 				}
 				else if((Math.abs(joR-baekR) > distance) || (joR+baekR < distance) || (joX==baekX && joY==baekY && baekR!=joR)) {
-					System.out.println(0);
+					bw.write("0");
+					bw.newLine();
 				}
 				else {
-					System.out.println(2);
+					bw.write("2");
+					bw.newLine();
 				}
 				
 			}
 			
+			bw.flush();
+			bw.close();
 			br.close();
 		}		
 }
