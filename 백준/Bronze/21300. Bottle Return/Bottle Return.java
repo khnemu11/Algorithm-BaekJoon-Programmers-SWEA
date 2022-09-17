14cf7e729cdf30682307bf08bd74fd1dec4136e4
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2022-09-17 20:48:46 +0900

    [Bronze V] Title: Bottle Return, Time: 120 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int result = 0;
+
+		while (st.hasMoreTokens()) {
+			result = result + Integer.valueOf(st.nextToken()) * 5;
+		}
+
+		bw.write(String.valueOf(result));
+		bw.newLine();
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
