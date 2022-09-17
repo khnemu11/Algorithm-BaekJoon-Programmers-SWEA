09ee1af31e29bfd89d625025b14a11b3e4e7169d
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2022-09-17 20:27:00 +0900

    [Bronze V] Title: 홍익대학교, Time: 208 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println(scan.nextInt()-1946);
+	
+	}
+}
