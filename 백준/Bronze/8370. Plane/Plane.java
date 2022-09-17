78ec39a80ce92aa96d98fa7ca8343cc82c104bf3
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2022-09-17 19:19:47 +0900

    [Bronze V] Title: Plane, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int result = Integer.valueOf(st.nextToken())*Integer.valueOf(st.nextToken()) + Integer.valueOf(st.nextToken())*Integer.valueOf(st.nextToken());
+		
+		bw.write(String.valueOf(result));
+		bw.newLine();
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
