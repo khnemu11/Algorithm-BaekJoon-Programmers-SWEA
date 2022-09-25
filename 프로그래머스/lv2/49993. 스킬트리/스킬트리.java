d106b487d6a73b983fe6f4710f1d4433486b722b
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2022-09-25 11:29:58 +0900

    [level 2] Title: 스킬트리, Time: 0.11 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -1,25 +1,31 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-		int result=0;
-		
-		for(String skill_tree : skill_trees) {
-			int current_skill=0;
-			
-			for(int i=0;i<skill_tree.length();i++) {
-				if(skill.indexOf(skill_tree.charAt(i)) != -1) {
-					if(skill_tree.charAt(i) == skill.charAt(current_skill)) {
-						current_skill++;
-					}
-					else {
-						current_skill=-1;
-						break;
-					}
-				}
-			}
-			if(current_skill!=-1) {
-				result++;
-			}
-		}
-        return result;
+        int answer = 0;
+        
+        for(int i=0;i<skill_trees.length;i++){
+            int count=0;
+            char arr [] = skill_trees[i].toCharArray();
+            boolean available=true;
+            
+            for(int j=0;j<arr.length;j++){
+                if(!skill.contains(String.valueOf(arr[j]))){
+                    continue;
+                }
+                if(arr[j]==skill.charAt(count)){
+                    count++;
+                }
+                else{
+                    available=false;
+                    break;
+                }
+            }
+            
+            if(available){
+                answer++;
+            }
+        }
+        
+        
+        return answer;
     }
 }
