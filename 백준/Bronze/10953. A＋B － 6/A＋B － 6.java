ff574709ee1d5c71a82b64183e65d9cd68dcba51
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2022-10-28 20:10:51 +0900

    [Bronze III] Title: A+B - 6, Time: 136 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = Integer.valueOf(br.readLine());
+		
+		while(T-->0) {
+			int sum = Arrays.stream(br.readLine().split(",")).mapToInt(x->Integer.valueOf(x)).sum();
+
+			bw.write(String.valueOf(sum));
+			bw.newLine();
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
