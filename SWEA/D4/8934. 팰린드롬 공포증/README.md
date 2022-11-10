6f2e270d7fc58627d0f6851505b1fd6a743e4a5f
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2022-11-10 13:49:45 +0900

    [D4] Title: 팰린드롬 공포증, Time: 187 ms, Memory: 35,440 KB -BaekjoonHub

---
+++ SWEA/D4/8934. 팰린드롬 공포증/README.md
@@ -0,0 +1,11 @@
+# [D4] 팰린드롬 공포증 - 8934 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AW5jJcZ68LsDFATQ) 
+
+### 성능 요약
+
+메모리: 35,440 KB, 시간: 187 ms, 코드길이: 909 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/8934. 팰린드롬 공포증/팰린드롬 공포증.java
@@ -0,0 +1,32 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+class Solution { 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T;
+		T=sc.nextInt();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			String word = sc.next();
+      		String result ="YES";
+            int [] abc = new int [3];
+
+            for(int i=0;i<word.length();i++){
+                abc[word.charAt(i)-'a']++;
+            }
+
+
+            if(Math.abs(abc[0]-abc[1])>1) result ="NO";
+            else if(Math.abs(abc[1]-abc[2])>1) result ="NO";
+            else if(Math.abs(abc[0]-abc[2])>1) result ="NO";
+
+      		bw.write("#" + test_case + " "+result+"\n");
+		}
+        bw.flush();
+        bw.close();
+    } 
+}
