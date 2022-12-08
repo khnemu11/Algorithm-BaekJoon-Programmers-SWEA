e1924c413d5bb4e7d859973b33ced31e78071c20
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2022-12-09 00:01:21 +0900

    [Gold III] Title: 별자리 만들기, Time: 148 ms, Memory: 14884 KB -BaekjoonHub

--- 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
@@ -1,91 +1,112 @@
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 public class Main {
-    
-    static ArrayList<Node> startList = new ArrayList<>();
-    static ArrayList<Edge> edgeList = new ArrayList<>(); 
-    static int[] parent;
-    static PriorityQueue<Edge> q = new PriorityQueue<>();
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
- 
-        int n = scan.nextInt();
-        for(int i = 0; i < n; i++) {
-            double x = scan.nextDouble();
-            double y = scan.nextDouble();
-            startList.add(new Node(i, x, y));
-        }
-        
-        //모든 간선 계산
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                double dist = Math.sqrt(Math.pow(startList.get(i).x - startList.get(j).x, 2) + Math.pow(startList.get(i).y - startList.get(j).y, 2));
-                q.add(new Edge(i, j, dist));
-            }
-        }
-        
-        parent = new int[startList.size()];
-        double result = kruskal();
-        System.out.printf("%.2f", result);
-    }
-    
-    public static double kruskal() {
-        for(int i = 0; i < startList.size(); i++) {
-            parent[i] = i;
-        }
-        
-        double dist = 0;
-        while(!q.isEmpty()) {
-            Edge current = q.poll();
-            
-            int p1 = find(current.start);
-            int p2 = find(current.end);
-            
-            if(p1 != p2) {
-                union(p1, p2);
-                dist += current.cost;
-            }
-        }
-        return dist;
-    }
-    
-    public static int find(int a) {
-        if(parent[a] == a) return a;
-        else return parent[a] = find(parent[a]);
-    }
-    
-    public static void union(int a, int b) {
-        parent[a] = b;
-    }
-    
-    public static class Node {
-        int num;
-        double x;
-        double y;
-        
-        public Node(int num, double x, double y) {
-            this.num = num;
-            this.x = x;
-            this.y = y;
-        }
-    }
-    
-    public static class Edge implements Comparable<Edge> {
-        int start;
-        int end;
-        double cost;
-        
-        public Edge(int start, int end, double cost) {
-            this.start = start;
-            this.end = end;
-            this.cost = cost;
-        }
-        
-        @Override
-        public int compareTo(Edge e) {
-            if(this.cost < e.cost) return -1;
-            return 1;
-        }
-    }
+	static int parent[];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int size = Integer.valueOf(br.readLine());
+
+		Coordinate coordinates[] = new Coordinate[size];
+
+		for (int i = 0; i < size; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			double x = Double.parseDouble(st.nextToken());
+			double y = Double.parseDouble(st.nextToken());
+			coordinates[i] = new Coordinate(x, y);
+		}
+		PriorityQueue<Distance> pq = new PriorityQueue<>();
+
+		for (int start = 0; start < size - 1; start++) {
+			for (int end = start + 1; end < size; end++) {
+				pq.add(new Distance(start, end, Math.sqrt(Math.pow(coordinates[start].row - coordinates[end].row, 2)
+						+ Math.pow(coordinates[start].col - coordinates[end].col, 2))));
+			}
+		}
+		parent = new int[size];
+
+		for (int i = 0; i < parent.length; i++) {
+			parent[i] = i;
+		}
+
+		double sum = 0;
+
+		while (!pq.isEmpty()) {
+			Distance curr = pq.poll();
+
+			if (find(curr.start) == find(curr.end)) {
+				continue;
+			}
+			union(find(curr.start), find(curr.end));
+			sum += curr.distance;
+		}
+		bw.write(String.format("%.2f", sum));
+		bw.newLine();
+		bw.flush();
+	}
+
+	public static int find(int curr) {
+		if (parent[curr] == curr) {
+			return curr;
+		} else {
+
+			return parent[curr] = find(parent[curr]);
+		}
+	}
+
+	public static void union(int a, int b) {
+		if (a != b) {
+			parent[a] = b;
+		}
+	}
+}
+
+class Coordinate {
+	double row;
+	double col;
+
+	public Coordinate(double row, double col) {
+		this.row = row;
+		this.col = col;
+	}
+
+	@Override
+	public String toString() {
+		return "Coordinate [row=" + row + ", col=" + col + "]";
+	}
+}
+
+class Distance implements Comparable<Distance> {
+	int start;
+	int end;
+	double distance;
+
+	public Distance(int start, int end, double distance) {
+		this.start = start;
+		this.end = end;
+		this.distance = distance;
+	}
+
+	@Override
+	public String toString() {
+		return "Distance [start=" + start + ", end=" + end + ", distance=" + distance + "]";
+	}
+
+	@Override
+	public int compareTo(Distance o) {
+		if (this.distance < o.distance) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
 }
