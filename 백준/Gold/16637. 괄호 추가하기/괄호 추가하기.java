a6b928bdf07c8f9b934ff2eded7a4c8e1a558126
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-01-28 18:17:02 +0900

    [Gold III] Title: 괄호 추가하기, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
@@ -1,68 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
-	static boolean visited[];
-	static int nums[];
-	static char ops[];
+	static char exp[];
 	static int max = Integer.MIN_VALUE;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int size = Integer.valueOf(br.readLine());
-		char[] input = br.readLine().toCharArray();
-		int numSize = size / 2 + 1;
+		exp = br.readLine().toCharArray();
 
-		visited = new boolean[numSize];
-		nums = new int[numSize];
-		ops = new char[numSize];
-
-		for (int i = 0; i < input.length; i += 2) {
-			nums[i / 2] = input[i] - '0';
-		}
-		for (int i = 1; i < input.length; i += 2) {
-			ops[i / 2] = input[i];
-		}
-
-		select(0);
+		select(0, exp[0] - '0');
 		System.out.println(max);
 
 	}
 
-	public static void select(int curr) {
-		if (curr >= ops.length - 1) {
-			Queue<Integer> numQ = new LinkedList<>();
-			Queue<Character> opQ = new LinkedList<>();
-
-			for (int i = 0; i < nums.length; i++) {
-				if (visited[i]) {
-					int val = calc(nums[i], nums[i + 1], ops[i]);
-					numQ.add(val);
-					i++;
-					opQ.add(ops[i]);
-				} else {
-					numQ.add(nums[i]);
-					opQ.add(ops[i]);
-				}
-			}
-			int sum = numQ.poll();
-
-			while (!numQ.isEmpty()) {
-				sum = calc(sum, numQ.poll(), opQ.poll());
-			}
-			max = Math.max(max, sum);
-
-			return;
+	public static void select(int curr, int sum) {
+		if (curr + 4 < exp.length) {
+			select(curr + 4, calc(sum, calc(exp[curr + 2] - '0', exp[curr + 4] - '0', exp[curr + 3]), exp[curr + 1]));
 		}
-
-		visited[curr] = true;
-		select(curr + 2);
-		visited[curr] = false;
-
-		select(curr + 1);
+		if (curr + 2 < exp.length) {
+			select(curr + 2, calc(sum, exp[curr + 2] - '0', exp[curr + 1]));
+		} else {
+			max = Math.max(max, sum);
+		}
 
 	}
 
