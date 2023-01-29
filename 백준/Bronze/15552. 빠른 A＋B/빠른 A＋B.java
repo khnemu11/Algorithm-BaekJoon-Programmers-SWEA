b289520db0c2a16e5a65cbc80edc4244ddf26b48
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-01-29 16:37:56 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1220 ms, Memory: 301196 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int T = Integer.parseInt(br.readLine());
+
+		while (T-- > 0) {
+			bw.write(String.valueOf(Arrays.stream(br.readLine().split(" ")).mapToInt(x -> Integer.parseInt(x)).sum()));
+			bw.newLine();
+		}
+
+		bw.flush();
+	}
+}
