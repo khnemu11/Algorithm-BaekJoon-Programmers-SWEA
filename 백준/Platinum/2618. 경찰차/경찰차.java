478737a58918c52f21b063f01b121abb982b32f3
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-02-05 15:48:33 +0900

    Update 경찰차.java
    경찰차 문제풀이 추가

--- 백준/Platinum/2618. 경찰차/경찰차.java
+++ 백준/Platinum/2618. 경찰차/경찰차.java
@@ -3,13 +3,25 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /*
 	풀이 알고리즘
-	경찰차와 목표지점 까지의 거리는 절대값(경찰차 행 - 목표지점 행) +절대값(경찰차 열 - 목표지점 열)
 	
+	보통 dp 문제 : 현재 위치의 최소값 = 이전 위치의 최소값 + min(이전 위치에서 경찰차 1이 움직인 거리, 이전 위치에서 경찰차 2가 움직인 거리);
+	하지만 마지막 경찰차의 2개의 위치가 각각 어디인지 알 수 있으므로 top-bottom 불가
+	단 시작 위치는 각각 1,1 N,N으로 정해져 있으므로 bottom-top으로 해결해야함
+	현재~마지막 까지의 최소값 = min(현재 위치에서 경찰차 1이 다음 사건까지 움직인 거리 + 다음 위치~마지막 까지의 최소값,현재 위치에서 경찰차 2이 다음 사건까지 움직인 거리 + 다음 위치~마지막 까지의 최소값)
+	경찰차와 목표지점 까지의 거리는 절대값(경찰차 행 - 목표지점 행) +절대값(경찰차 열 - 목표지점 열)
+	경찰차의 위치 == 사건의 좌표이므로 현재 몇번째 사건인지 알면 각 경찰차의 좌표를 알 수 있다.
+	
+	dp가 끝난 후 경로 추적시 현재~마지막 까지의 최소값의 점화식을 이용한다.
+	현재 위치에서 경찰차 1이 다음 사건까지 움직인 거리 + 다음 위치~마지막 까지의 최소값 != 현재~마지막 까지의 최소값 -> 현재 위치에선 2를 움직인 것이 최소
+	현재 위치에서 경찰차 1이 다음 사건까지 움직인 거리 + 다음 위치~마지막 까지의 최소값 == 현재~마지막 까지의 최소값 -> 현재 위치에선 1를 움직인 것이 최소
+	
+	bottom-top으로 순서대로 출력하면 해결
+	
+	풀이 팁 : dp문제에서 bottom-top vs top-bottom의 선택 기준은 시작 위치가 명확한지 vs 목표 위치가 명확한지이다.
 */
 
 public class Main {
@@ -55,22 +67,27 @@ public class Main {
 		bw.flush();
 	}
 
-	public static int getMinDistance(int curr, int police1, int police2) {
-		if (curr > W) {
-			return 0;
-		} else if (dp[police1][police2] != 0) {
+	public static int getMinDistance(int curr, int police1, int police2) { //사건 curr번 이고 경찰의 위치가 police1,police2 일때 마지막 사건 까지의 최소값
+		if (curr > W) { //사건 번호가 주어진 개수보다 커졌을때 -> 모든 사건을 끝냈을 때
+			return 0; //경찰차를 안움직여도 되므로 움직인 거리는 0이다.
+		} else if (dp[police1][police2] != 0) { // 이미 최소값을 구했을 때
 			return dp[police1][police2];
 		} else {
-			Coordinate police1Coord = police1 == 0 ? new Coordinate(1, 1) : events[police1]; // 현재 경찰1위치
-			Coordinate police2Coord = police2 == 0 ? new Coordinate(N, N) : events[police2]; // 현재 경찰 2
+			Coordinate police1Coord = police1 == 0 ? new Coordinate(1, 1) : events[police1]; // 현재 경찰 1 위치 : 0은 초기값이므로 경찰1의 위치는 1,1
+			Coordinate police2Coord = police2 == 0 ? new Coordinate(N, N) : events[police2]; // 현재 경찰 2 위치 : 0은 초기값이므로 경찰1의 위치는 N,N
+			
+			//min(현재 위치에서 police1이 출동했을 때,현재 위치에서 police2가 출동했을 때)
+			//-> min(다음 사건에서 경찰의 위치가 police1이 [현재 사건위치]이고 police 2가 움직이지 않았을때 최소거리 + 사건번호 curr로 움직인 거리, 
+			//다음 사건에서 경찰의 위치가 police2가 [현재 사건위치]이고 police 1가 움직이지 않았을때 최소거리 + 사건번호 curr로 움직인 거리)
 			dp[police1][police2] = Math.min(
 					getMinDistance(curr + 1, curr, police2) + getDistance(police1Coord, events[curr]),
 					getMinDistance(curr + 1, police1, curr) + getDistance(police2Coord, events[curr]));
+					
 			return dp[police1][police2];
 		}
 	}
 
-	public static int getDistance(Coordinate from, Coordinate to) {
+	public static int getDistance(Coordinate from, Coordinate to) { //두 좌표간의 거리를 구하는 매소드
 		return Math.abs(from.row - to.row) + Math.abs(from.col - to.col);
 	}
 }
