535433754625f4a37ea6542828d8f636f43ff3a3
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-02-06 18:36:30 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 584 ms, Memory: 148416 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -5,47 +5,68 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
-public class Main {
-	static char star[][];
+/*
+ * 
+ * 풀이 알고리즘
+   각 윗점을 기준으로 위, 왼쪽아래, 오른쪽 아래로 다시 분할하는 형태로 이루어져 있다.
+   높이=3, 밑변의 길이= 5,높이=6, 밑변의 길이=11,높이=12, 밑변의 길이= 23 ... 높이 = N, 밑변의 길이 = N*2-1
+   각 점을 담을 배열의 크기는 N X (N*2-1)
+   최초 윗점의 좌표는 (0,(N*2-1)/2)
+   다음 윗점 : (현재 윗점의 행,현재높이/2),  다음 왼쪽아래 : (현재 윗점의 행-현재높이/2,현재높이/2), 다음 오른쪽 아래 : (현재 윗점의 행+현재높이/2,현재높이/2)
+   최소 높이는 3이므로 높이가 3이 될때 기본 삼각형값을 배열에 입력
+*/
 
-	public static void main(String[] args) throws IOException {
+public class Main {
+	static char map[][];
+	static int N;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.valueOf(br.readLine());
-		int k = (int) (Math.log(N / 3) / Math.log(2));
-		star = new char[N][2 * N - 1];
+		N = Integer.valueOf(br.readLine());
+		map = new char[N][N * 2 - 1]; //배열의 크기는 점화식에 따라 N X (N*2-1)
 
-		for (int i = 0; i < N; i++) {
-			Arrays.fill(star[i], ' ');
+		for (int i = 0; i < map.length; i++) {
+			Arrays.fill(map[i], ' '); //공백으로 배열 초기화
 		}
-		draw(0, star[0].length / 2, k);
 
-		for (int i = 0; i < star.length; i++) {
-			for (int j = 0; j < star[0].length; j++) {
-				bw.write(star[i][j]);
+		draw(new Coordinate(0, map[0].length / 2), N); //시작 윗점은 첫번째 행의 중간 열
+
+		for (int i = 0; i < map.length; i++) {
+			StringBuilder sb = new StringBuilder();
+			for (int j = 0; j < map[0].length; j++) {
+				sb.append(map[i][j]);
 			}
-			bw.newLine();
+			bw.write(sb.toString() + "\n");
 		}
+
 		bw.flush();
-		br.close();
 	}
 
-	static public void draw(int row, int col, int k) {
-		if (k == 0) {
-			star[row][col] = '*';
-
-			star[row + 1][col + 1] = '*';
-			star[row + 1][col - 1] = '*';
+	public static void draw(Coordinate top, int height) { //윗점을 기준으로 삼각형을 재귀로 탐색하여 그리는 매소드
+		if (height == 3) { //최소크기의 삼각형일 때 *입력
+			map[top.row][top.col] = '*';
+			map[top.row + 1][top.col - 1] = '*';
+			map[top.row + 1][top.col + 1] = '*';
 
 			for (int i = -2; i < 3; i++) {
-				star[row + 2][col + i] = '*';
+				map[top.row + 2][top.col + i] = '*';
 			}
 		} else {
-			int length = (int) Math.pow(2, k) * 3;
-			draw(row, col, k - 1);
-			draw(length / 2 + row, col - length / 2, k - 1);
-			draw(length / 2 + row, col + length / 2, k - 1);
+			int nextHeight = height / 2; //다음 높이
+			draw(top, nextHeight); //윗점
+			draw(new Coordinate(top.row + nextHeight, top.col - nextHeight), nextHeight); //왼쪽 아래
+			draw(new Coordinate(top.row + nextHeight, top.col + nextHeight), nextHeight); //오른쪽 아래
 		}
 	}
+}
 
+class Coordinate {
+	int row;
+	int col;
+
+	public Coordinate(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
 }
