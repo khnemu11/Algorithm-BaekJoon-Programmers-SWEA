4f26665fe59b35141317e82cc8495e23b2a18d3f
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-02-07 21:23:10 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 110 ms, Memory: 18,632 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,75 +1,69 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
-import java.io.FileInputStream;
-import java.util.*;
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		/*
-		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
-		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
-		 */
-		//System.setIn(new FileInputStream("res/input.txt"));
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-		/*
-		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
-		 */
-			Scanner sc = new Scanner(System.in);
+/*
+ * 풀이 과정
+ * 
+ * 매번 정렬을해서 판단하면 n*nlogn으로 시간 초과 우려가 있음
+ * 각 높이를 인덱스로 해서 높이의 개수가 담긴 배열을 이용해 투포인터로 이용해 작은값은 더해주고 큰값은 빼주는 형태로 순회
+ * 
+ * 예를 들어 1,1,5,7이라면 높이 배열은 0,2,0,0,0,1,0,1이 되고 왼쪽 포인터(최소값)는 1, 오른쪽 포인터는 7(최대값)이됨
+ * 평탄화를 진행하면 최소값을 가지고 있는 높이의 개수를 1을 감소시키고 최소값+1의 높이의 개수를 1 중가
+ * 똑같이 최대값을 가지고 있는 높이의 개수를 1을 감소시키고 최대값-1의 높이의 개수를 1 중가
+ * 만약 최대값과 최소값이 같으면 더이상 평탄화가 불가능 하므로 반복문 탈출
+ * */
+
+public class Solution {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		int T = 10;
+		int MAX_HEIGHT = 100;
+		int MIN_HEIGHT = 1;
 
 		for (int test_case = 1; test_case <= T; test_case++) {
-			int dump = sc.nextInt();
-			int nums[] = new int[100];
+			int dump = Integer.valueOf(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int min = MAX_HEIGHT;
+			int max = MIN_HEIGHT;
+			int heights[] = new int[MAX_HEIGHT + 1];
 
-			for (int i = 0; i < nums.length; i++) {
-				int num = sc.nextInt();
-
-				nums[i] = num;
+			while (st.hasMoreTokens()) {
+				int height = Integer.valueOf(st.nextToken());
+				min = Math.min(min, height);
+				max = Math.max(max, height);
+				heights[height]++;
 			}
-
-			Arrays.sort(nums);
 
 			while (dump-- > 0) {
-				nums[0]++;
-				nums[99]--;
-				Arrays.sort(nums);
+				heights[min]--;
+				heights[min + 1]++;
+				heights[max]--;
+				heights[max - 1]++;
+
+				while (heights[min] == 0) {
+					min++;
+				}
+				while (heights[max] == 0) {
+					max--;
+				}
+
+				if (min == max) {
+					break;
+				}
 			}
 
-			System.out.println("#" + test_case + " " + (nums[99] - nums[0]));
+			StringBuilder sb = new StringBuilder();
+
+			sb.append("#").append(test_case).append(" ").append(max - min).append("\n");
+			bw.write(sb.toString());
 		}
+
+		bw.flush();
 	}
 }
