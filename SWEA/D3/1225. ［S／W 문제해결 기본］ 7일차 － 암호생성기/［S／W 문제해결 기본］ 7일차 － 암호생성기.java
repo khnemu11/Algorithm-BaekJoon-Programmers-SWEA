d651fb3aa86bba2e61c6be686d16b4d8a16631fc
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-02-10 18:45:42 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 188 ms, Memory: 28,132 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,49 +1,59 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class Solution {
-	public static void main(String arg[]) {
-		Scanner sc = new Scanner(System.in);
-		int test_case = 10;
-		while (test_case-- > 0) {
-			int no = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = 10;
 
-			Queue<Integer> queue = new LinkedList<>();
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int N = Integer.valueOf(br.readLine());
+			int nums[] = Arrays.stream(br.readLine().split(" ")).mapToInt(x -> Integer.valueOf(x)).toArray();
 
-			int size = 8;
+			Queue<Integer> q = new LinkedList<>();
 
-			while (size-- > 0) {
-				queue.add(sc.nextInt());
+			for (int num : nums) {
+				q.add(num);
 			}
-			int decrease = 1;
-			while (true) {
-				int num = queue.poll();
+			boolean exit = false;
+			
+			while (!exit) {
+				int loop = 5;
+				int weight = 1;
 
-				if (num - decrease > 0) {
-					num -= decrease;
-				} else {
-					num = 0;
-				}
-
-				queue.add(num);
-
-				if (num == 0) {
-					break;
-				}
-				decrease++;
-				if (decrease > 5) {
-					decrease = 1;
+				while (loop-- > 0) {
+					int curr = q.poll();
+					if (curr - weight <= 0) {
+						q.add(0);
+						exit = true;	
+						break;
+					} else {
+						q.add(curr - weight);
+					}
+					weight++;
 				}
 			}
-			StringBuilder result = new StringBuilder("#");
-			result.append(no).append(" ");
-			while (!queue.isEmpty()) {
-				result.append(queue.poll());
-				result.append(" ");
+
+			StringBuilder sb = new StringBuilder();
+
+			sb.append("#").append(test_case).append(" ");
+
+			while (!q.isEmpty()) {
+				sb.append(q.poll()).append(" ");
 			}
-			result.deleteCharAt(result.length() - 1);
-			System.out.println(result.toString());
+			sb.deleteCharAt(sb.length() - 1).append("\n");
+
+			bw.write(sb.toString());
 		}
+
+		bw.flush();
 	}
 }
