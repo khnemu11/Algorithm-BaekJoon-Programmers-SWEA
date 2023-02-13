d5c38f3b44472911505d6033a449e6fe1758f7f2
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-02-13 14:13:16 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 228 ms, Memory: 17880 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,40 +1,62 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
+/*
+	풀이 알고리즘
+	큐로 푸는것이 정석이나 링크드 리스트로 해결
+	노드를 만들어서 링크드리스트 구현
+*/
+
 public class Main {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		Queue<Integer> queue = new LinkedList<>();
-		int size = Integer.valueOf(st.nextToken());
-		int seq = Integer.valueOf(st.nextToken());
+		String[] input = br.readLine().split(" ");
+		int N = Integer.valueOf(input[0]);
+		int K = Integer.valueOf(input[1]);
 
-		for (int i = 1; i <= size; i++) {
-			queue.add(i);
+		Node root = new Node();
+		Node curr = root;
+
+		for (int i = 1; i <= N; i++) {
+			curr.next = new Node(i);
+			curr = curr.next;
 		}
-		String result [] = new String[size];
-		for(int k=0;k<size;k++) {
-			for (int i = 0; i < seq - 1; i++) {
-				int num = queue.poll();
-				queue.add(num);
+
+		Node prev = root.next;
+		curr.next = root.next;
+
+		int idx = 0;
+		int nums[] = new int[N];
+
+		while (idx < N) {
+			for (int move = 0; move < K; move++) {
+				prev = curr;
+				curr = curr.next; //노드 이동
 			}
-			result[k] = String.valueOf(queue.poll());
+			nums[idx] = curr.val; 
+			prev.next = curr.next; //현재 노드 삭제 
+			idx++;
 		}
-		
-		String answer = Arrays.stream(result).collect(Collectors.joining(", ","<", ">"));
-		
-		bw.write(answer);
-		bw.newLine();
+
+		bw.write(Arrays.stream(nums).mapToObj(x -> String.valueOf(x)).collect(Collectors.joining(", ", "<", ">"))); //joining을 이용한 문자열 만들기
 		bw.flush();
-		bw.close();
-		br.close();
+	}
+}
+
+class Node {
+	int val;
+	Node next;
+
+	public Node() {
+	}
+
+	public Node(int val) {
+		this.val = val;
 	}
 }
