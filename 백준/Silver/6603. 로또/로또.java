45fc1e946f78c6794eb82f00bfbbbf9a1fb20242
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-02-16 12:14:47 +0900

    [Silver II] Title: 로또, Time: 160 ms, Memory: 15016 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,54 +1,60 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
+/*
+	풀이 알고리즘
+	주어진 숫자 중 6개를 고르면 되는 완전탐색 문제
+*/
 
 public class Main {
-	static boolean visited[];
+	static int nums[];
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int k = 6;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		while (true) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int k = Integer.valueOf(st.nextToken());
-			if (k == 0) {
+
+			int size = Integer.valueOf(st.nextToken());
+
+			if (size == 0) {
 				break;
 			}
 
-			int set[] = new int[k];
-			visited = new boolean[k];
-			for (int i = 0; i < k; i++) {
-				set[i] = Integer.valueOf(st.nextToken());
+			nums = new int[size];
+
+			for (int i = 0; i < nums.length; i++) {
+				nums[i] = Integer.valueOf(st.nextToken());
 			}
-
-			dfs(6, 0, set, 0);
-
-			System.out.println();
+			pick(0, new ArrayList<Integer>(), 0);
+			bw.write("\n");
 		}
 
-		br.close();
+		bw.flush();
 	}
 
-	static void dfs(int depth, int curr, int num[], int max) {
-		if (depth == curr) {
+	public static void pick(int idx, ArrayList<Integer> selected, int cnt) throws IOException {
+		if (cnt == k) {
 			StringBuilder sb = new StringBuilder();
 
-			for (int i = 0; i < visited.length; i++) {
-				if (visited[i]) {
-					sb.append(num[i]);
-					sb.append(" ");
-				}
+			for (int num : selected) {
+				sb.append(num).append(" ");
 			}
 
-			System.out.println(sb.toString());
+			bw.write(sb.deleteCharAt(sb.length() - 1).toString() + "\n");
+
+			return;
 		} else {
-			for (int i = max; i < visited.length; i++) {
-				if (!visited[i]) {
-					visited[i] = true;
-					dfs(depth, curr + 1, num, i + 1);
-					visited[i] = false;
-				}
+			for (int i = idx; i < nums.length; i++) {
+				selected.add(nums[i]);
+				pick(i + 1, selected, cnt + 1);
+				selected.remove(selected.size() - 1);
 			}
 		}
 	}
