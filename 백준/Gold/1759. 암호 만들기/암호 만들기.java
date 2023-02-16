488a2b0b4b21152065c7de3deedbf5e29796c352
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-02-16 12:07:57 +0900

    [Gold V] Title: 암호 만들기, Time: 152 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -4,66 +4,72 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
+/*
+	풀이 알고리즘
+	알파벳 순서로 출력해야 하므로 입력값을 알파벳 오름차순 정렬
+	선택한 알파벳의 개수 - 선택한 모음의 개수 < 2 -> 선택한 자음의 개수가 2개 보다 작음
+	dfs로 완전 탐색 실행
+*/
 
 public class Main {
-	static boolean visited[];
-	static List<String> vowels = Arrays.asList("a", "e", "i", "o", "u");
+	static int arr[][];
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static String vowels = "aeiou";
+	static String alpha[];
+	static int vowelCnt;
+	static int L;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int length = Integer.valueOf(st.nextToken());
-		int size = Integer.valueOf(st.nextToken());
+		L = Integer.valueOf(st.nextToken()); // 문자의 길이
+		int C = Integer.valueOf(st.nextToken()); // 알파벳 개수
 
-		visited = new boolean[size];
+		st = new StringTokenizer(br.readLine());
+		alpha = new String[C];
 
-		String arr[] = br.readLine().split(" ");
+		for (int i = 0; i < C; i++) {
+			alpha[i] = st.nextToken();
+		}
+		Arrays.sort(alpha);
 
-		Arrays.sort(arr);
-
-		dfs(0, 0, length, 0, 0, arr);
-
-		bw.close();
-		br.close();
+		pick(0, new StringBuilder(), 0);
+		bw.flush();
 	}
 
-	public static void dfs(int vowel, int consonant, int depth, int curr, int index, String arr[]) {
-		if (depth == curr) {
-			if (vowel > 0 && consonant > 1) {
-				for (int i = 0; i < arr.length; i++) {
-					if (visited[i]) {
-						System.out.print(arr[i]);
-					}
-				}
-				System.out.println();
+	public static void pick(int idx, StringBuilder sb, int cnt) throws IOException {
+		if (cnt == L) {
+			if (L - vowelCnt < 2 || vowelCnt < 1) { // 자음의 개수가 2개 미만이고 모음의 개수가 1개 미만이면 패스
+				return;
+			} else {
+				bw.write(sb.toString() + "\n");
+				return;
 			}
 		} else {
-			for (int i = index; i < arr.length; i++) {
-				if (visited[i]) {
-					continue;
-				}
+			for (int i = idx; i < alpha.length; i++) {
+				sb.append(alpha[i]);
 
-				visited[i] = true;
-				if (vowels.contains(arr[i])) {
-					vowel++;
-					
-					dfs(vowel, consonant, depth, curr + 1, i + 1, arr);
-
-					visited[i] = false;
-					vowel--;
+				if (vowels.contains(alpha[i])) { // 모음이 포함되면 모음 개수 증가
+					vowelCnt++;
+					pick(i + 1, sb, cnt + 1);
+					vowelCnt--;
 				} else {
-					consonant++;
-					
-					dfs(vowel, consonant, depth, curr + 1, i + 1, arr);
-
-					visited[i] = false;
-					consonant--;
+					pick(i + 1, sb, cnt + 1);
 				}
-
+				sb.deleteCharAt(sb.length() - 1);
 			}
 		}
+	}
+}
+
+class Coordinate {
+	int row;
+	int col;
+
+	public Coordinate(int row, int col) {
+		this.row = row;
+		this.col = col;
 	}
 }
