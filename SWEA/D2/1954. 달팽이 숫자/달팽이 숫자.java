e52a2269dfce6c59664704d80925b84358dae73a
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-02-20 17:38:46 +0900

    [D2] Title: 달팽이 숫자, Time: 105 ms, Memory: 19,156 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -3,20 +3,15 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-
-/*
- * 풀이 과정
- * 
- * 시계방향(오른쪽->아래->왼쪽->위)으로 탐색
- * 이때 이미 탐색했거나 배열 이외의 좌표인경우 다음 방향으로 가도록 설정한다.
- * */
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
 		int T = Integer.valueOf(br.readLine());
+
 		for (int test_case = 1; test_case <= T; test_case++) {
 			int N = Integer.valueOf(br.readLine());
 			int map[][] = new int[N][N];
@@ -25,42 +20,32 @@ public class Solution {
 			int leftRight[] = { 1, 0, -1, 0 };
 
 			Coordinate curr = new Coordinate(0, 0);
-
-			int move = 0;
-
-			for (int i = 1; i <= N * N; i++) {
-				visited[curr.row][curr.col] = true;
-				map[curr.row][curr.col] = i;
-				Coordinate next = new Coordinate(curr.row + upDown[move], curr.col + leftRight[move]);
-				if (!isValid(next, map, visited)) {
-					move = move + 1 == 4 ? 0 : move + 1;
-					next = new Coordinate(curr.row + upDown[move], curr.col + leftRight[move]);
+			map[0][0] = 1;
+			int direction = 0;
+			visited[0][0] = true;
+			for (int move = 2; move <= Math.pow(N, 2); move++) {
+				Coordinate next = new Coordinate(curr.row + upDown[direction], curr.col + leftRight[direction]);
+				if (next.row < 0 || next.col < 0 || next.row >= N || next.col >= N || visited[next.row][next.col]) {
+					direction = direction + 1 >= 4 ? 0 : direction + 1;
+					next = new Coordinate(curr.row + upDown[direction], curr.col + leftRight[direction]);
 				}
+				visited[next.row][next.col] = true;
+				map[next.row][next.col] = move;
 				curr = next;
 			}
-
 			StringBuilder sb = new StringBuilder();
-
 			sb.append("#").append(test_case).append("\n");
-			bw.write(sb.toString());
 			for (int i = 0; i < map.length; i++) {
-				sb.setLength(0);
-				for (int j = 0; j < map[0].length; j++) {
+				for (int j = 0; j < map.length; j++) {
 					sb.append(map[i][j]).append(" ");
 				}
-				bw.write(sb.deleteCharAt(sb.length() - 1).append("\n").toString());
+				sb.deleteCharAt(sb.length() - 1);
+				sb.append("\n");
 			}
-		}
 
+			bw.write(sb.toString());
+		}
 		bw.flush();
-	}
-
-	public static boolean isValid(Coordinate coord, int map[][], boolean visited[][]) {
-		if (coord.row < 0 || coord.row >= map.length || coord.col < 0 || coord.col >= map[0].length
-				|| visited[coord.row][coord.col]) {
-			return false;
-		}
-		return true;
 	}
 }
 
