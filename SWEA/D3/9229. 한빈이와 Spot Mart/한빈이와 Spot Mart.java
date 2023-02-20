b42865860a658af70606cd32c9e81ee6a289c375
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-02-20 17:00:26 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 226 ms, Memory: 33,948 KB -BaekjoonHub

--- SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -1,53 +1,44 @@
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int T = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.valueOf(br.readLine());
 
 		for (int test_case = 1; test_case <= T; test_case++) {
-			int N = sc.nextInt();
-			int limit = sc.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.valueOf(st.nextToken());
+			int weight = Integer.valueOf(st.nextToken());
+			int snacks[] = Arrays.stream(br.readLine().split(" ")).mapToInt(x -> Integer.valueOf(x)).toArray();
+			int max = 0;
 
-			int weight[] = new int[N];
-
-			for (int i = 0; i < N; i++) {
-				weight[i] = sc.nextInt();
-			}
-
-			Arrays.sort(weight);
-
-			int l = N - 2;
-			int r = N - 1;
-			int max = -1;
-
-			while (r > 0) {
-				if (weight[l] + weight[r] <= limit && weight[l] + weight[r] > max) {
-					max = weight[l] + weight[r];
-					r--;
-					l = r - 1;
-                    continue;
-				}
-
-				l--;
-
-				if (l == -1) {
-					r--;
-					l = r - 1;
+			for (int i = 0; i < snacks.length - 1; i++) {
+				for (int j = i + 1; j < snacks.length; j++) {
+					int sum = snacks[i] + snacks[j];
+					if (sum <= weight) {
+						max = Math.max(max, sum);
+					}
 				}
 			}
 
 			StringBuilder sb = new StringBuilder();
-			sb.append("#").append(test_case).append(" ").append(max);
 
-			System.out.println(sb.toString());
+			sb.append("#").append(test_case).append(" ");
 
+			if (max == 0) {
+				sb.append(-1).append("\n");
+			} else {
+				sb.append(max).append("\n");
+			}
+			bw.write(sb.toString());
 		}
-
+		bw.flush();
 	}
-
 }
