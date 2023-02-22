9d8e8a218d2d020e3bac6efbf41dad25f5b383db
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-02-22 13:31:47 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 1,402 ms, Memory: 20,264 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -3,58 +3,70 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class Solution{
+/*풀이 알고리즘
+ * 최적의 경로를 찾지 말고 모든 경로를 탐색하라고 명시하였으므로 브루트 포스 사용
+ * 시작 좌표를 시작으로 모든 고객의 집을 한번씩 방문하고 집 좌표로 이동 -> 10P10 = 10! = 3,628,800(각 테스트 케이스 별로 2초이내어야 하므로 가능)
+ * */
+
+public class Solution {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static Coordinate company;
+	static Coordinate house;
+	static Coordinate customers[];
 	static boolean visited[];
-	static Coordinate house[];
 	static int min;
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int T = Integer.valueOf(br.readLine());
 
-		for (int test_case = 1; test_case <= T; test_case++) {
-			int size = Integer.valueOf(br.readLine());
-			house = new Coordinate[size];
-			visited = new boolean[size];
-			min = Integer.MAX_VALUE;
-			StringTokenizer st = new StringTokenizer(br.readLine());
-
-			Coordinate start = new Coordinate(Integer.valueOf(st.nextToken()), Integer.valueOf(st.nextToken()));
-			Coordinate end = new Coordinate(Integer.valueOf(st.nextToken()), Integer.valueOf(st.nextToken()));
-
-			for (int i = 0; i < house.length; i++) {
-				house[i] = new Coordinate(Integer.valueOf(st.nextToken()), Integer.valueOf(st.nextToken()));
-			}
-			FindMin(0, 0, start, end);
+		for (int testcase = 1; testcase <= T; testcase++) {
+			init();
+			selectCustomer(company, 0, 0);
 
 			StringBuilder sb = new StringBuilder();
+			sb.append("#").append(testcase).append(" ").append(min).append("\n");
 
-			sb.append("#" + test_case + " " + min);
 			bw.write(sb.toString());
-			bw.newLine();
 		}
 
 		bw.flush();
+		bw.close();
 	}
 
-	public static void FindMin(int depth, int sum, Coordinate curr, Coordinate end) {
-		if (depth == house.length) {
-			int distance = Math.abs(curr.row - end.row) + Math.abs(curr.col - end.col);
-			min = Math.min(distance + sum, min);
-			return;
-		}
+	public static void init() throws IOException { //최소값, 집, 회사, 고객 좌표 저장
+		int N = Integer.valueOf(br.readLine());
+		customers = new Coordinate[N];
+		visited = new boolean[N];
+		min = Integer.MAX_VALUE;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		for (int i = 0; i < house.length; i++) {
-			if (visited[i]) {
-				continue;
+		company = new Coordinate(Integer.valueOf(st.nextToken()), Integer.valueOf(st.nextToken()));
+		house = new Coordinate(Integer.valueOf(st.nextToken()), Integer.valueOf(st.nextToken()));
+
+		for (int i = 0; i < N; i++) {
+			customers[i] = new Coordinate(Integer.valueOf(st.nextToken()), Integer.valueOf(st.nextToken()));
+		}
+	}
+
+	public static void selectCustomer(Coordinate curr, int count, int distance) { //고객을 선택하고 마지막에 집을 선택해 거리를 구하는 메소드 (현재 위치, 고른 고객의 수, 현재까지 이동한 거리)
+		if (count == customers.length) {
+			distance = distance + Math.abs(curr.row - house.row) + Math.abs(curr.col - house.col);
+			min = Math.min(min, distance);
+		} else {
+			for (int i = 0; i < customers.length; i++) {
+				if (visited[i]) {
+					continue;
+				}
+
+				visited[i] = true;
+				selectCustomer(customers[i], count + 1,
+						distance + Math.abs(curr.row - customers[i].row) + Math.abs(curr.col - customers[i].col));
+				visited[i] = false;
 			}
-			visited[i] = true;
-			int distance = Math.abs(curr.row - house[i].row) + Math.abs(curr.col - house[i].col);
-			FindMin(depth + 1, sum + distance, house[i], end);
-			visited[i] = false;
 		}
 	}
 }
