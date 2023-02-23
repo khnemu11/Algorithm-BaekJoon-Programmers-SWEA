a08237e2effc24e9005f08a897b995c55f3c5cd7
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-02-23 15:45:43 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 공통조상, Time: 205 ms, Memory: 34,956 KB -BaekjoonHub

--- SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
+++ SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
@@ -1,105 +1,89 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
+/*
+	풀이 알고리즘
+	노드의 개수는 최대 10만개
+	왼쪽, 오른쪽 노드 필요
+	한 노드부터 시작하여 부모를 찾고 아직 방문하지 않은 자식이 있다면 자식 탐색
+	자식중에 나머지 찾으려는 노드가 있다면 여태까지 방문한 횟수 리턴
+	아니라면 부모로 올라감
+*/
 
 public class Solution {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int T = Integer.valueOf(br.readLine());
+	static Node nodes[];
+	static int parents[];
+	static boolean visited[];
+	static int length;
 
-		for (int test_case = 1; test_case <= T; test_case++) {
-			int row[] = Arrays.stream(br.readLine().split(" ")).mapToInt(x -> Integer.valueOf(x)).toArray();
-			int V = row[0];
-			int E = row[1];
-			int aVal = row[2];
-			int bVal = row[3];
-			int parentArr[] = new int[V + 1];
-			Node tree[] = new Node[V + 1]; // 왼쪽, 오른쪽 자식으로 바로 탐색이 가능하게 링크드 리스트가 아니라 배열로 표현
-			boolean visited[] = new boolean[V + 1];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int TC = Integer.parseInt(br.readLine());
+
+		for (int testcase = 1; testcase <= TC; testcase++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
-			for (int i = 0; i < tree.length; i++) {
-				tree[i] = new Node(i);
-			}
+			int V = Integer.parseInt(st.nextToken()); // 정점 개수
+			int E = Integer.parseInt(st.nextToken()); // 간선 개수
+			int node1 = Integer.parseInt(st.nextToken());
+			int node2 = Integer.parseInt(st.nextToken());
 
-			parentArr[1] = 1;
-			// 트리에 자식을 넣고 각 노드 별 부모 배열 추가
-			for (int i = 0; i < E; i++) {
-				int parent = Integer.valueOf(st.nextToken());
-				int child = Integer.valueOf(st.nextToken());
+			nodes = new Node[V + 1];
+			parents = new int[V + 1];
+			visited = new boolean[V + 1];
 
-				parentArr[child] = parent;
-				tree[parent].add(new Node(child));
-			}
+			st = new StringTokenizer(br.readLine());
+			parents[1] = 1; // 1은 항상 루트이므로
+			while (st.hasMoreTokens()) { // 입력받는 부모-자식 노드 생성 및 연결
+				int parent = Integer.parseInt(st.nextToken());
+				int child = Integer.parseInt(st.nextToken());
 
-			// 목표 노드의 부모 노드를 체크
-			int curr = aVal;
-			visited[curr] = true;
+				parents[child] = parent;	//자식의 부모 최신화
 
-			while (parentArr[curr] != curr) {
-				visited[curr] = true;
-				curr = parentArr[curr];
-			}
-
-			curr = bVal;
-
-			while (!visited[curr] && parentArr[curr] != curr) {
-				curr = parentArr[curr];
-			}
-
-			int sameParent = curr;
-
-			// 자식 노드 개수 세기
-			int cnt = 0;
-			Queue<Node> q = new LinkedList<>();
-			q.add(tree[sameParent]);
-
-			while (!q.isEmpty()) {
-				cnt++;
-				Node currNode = q.poll();
-
-				if (currNode.left != null) {
-					q.add(tree[currNode.left.val]);
+				if (nodes[parent] == null) {	//자식이나 부모 객체가 없으면 생성
+					nodes[parent] = new Node();
 				}
-				if (currNode.right != null) {
-					q.add(tree[currNode.right.val]);
+				if (nodes[child] == null) {
+					nodes[child] = new Node();
+				}
+
+				if (nodes[parent].left == 0) {	//왼쪽부터 부모의 자식을 삽입
+					nodes[parent].left = child;	
+				} else {
+					nodes[parent].right = child;
 				}
 			}
 
-			StringBuilder sb = new StringBuilder();
-			sb.append("#" + test_case + " " + sameParent + " " + cnt);
-			bw.write(sb.toString());
-			bw.newLine();
+			goToParent(node1);	//노드 1개를 부모까지 탐색
+			int commonParent = goToParent(node2); //나머지 노드를 부모까지 탐색하고 이미 탐색한 노드라면 해당 노드가 공통 부모이므로 리턴
+			int length = getLength(commonParent); //공통 부모로 길이 계산
+
+			System.out.println("#" + testcase + " " + commonParent + " " + length);
 		}
 
-		bw.flush();
+		br.close();
+	}
+
+	public static int goToParent(int child) { //부모로 탐색하는 메소드 (자식 노드)
+		if (visited[child]) {
+			return child;
+		} else {
+			visited[child] = true;
+			return goToParent(parents[child]);
+		}
+	}
+
+	public static int getLength(int parent) { //노드의 서브트리를 구하는 메소드 (노드 값)
+		if (parent == 0) { //노드가 비어있는경우
+			return 0;
+		} else { //왼쪽, 오른쪽, 자기 자신(1)을 리턴
+			return getLength(nodes[parent].left) + getLength(nodes[parent].right) + 1;
+		}
 	}
 }
 
 class Node {
-	int val;
-	Node left;
-	Node right;
-
-	public Node() {
-	}
-
-	public Node(int val) {
-		this.val = val;
-	}
-
-	public void add(Node curr) {
-		if (left == null) {
-			this.left = curr;
-		} else {
-			this.right = curr;
-		}
-	}
+	int left;
+	int right;
 }
