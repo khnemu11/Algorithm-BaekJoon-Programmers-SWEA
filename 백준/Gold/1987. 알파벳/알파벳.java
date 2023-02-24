600b07489ffeba99ea8e6ca718488a1bb675ef2a
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-02-24 11:37:20 +0900

    [Gold IV] Title: 알파벳, Time: 1300 ms, Memory: 297008 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -3,56 +3,60 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
 
+/*
+	풀이 알고리즘
+	4방향으로 방문하지 않은 알파벳만 방문하여 백트래킹-dfs로 해결
+*/
 public class Main {
-	static boolean visited[];
-	static char board[][];
-	static int max;
+	static int maxMove = 0;
+	static char map[][];
+	static boolean visited[] = new boolean[26];
+	static int upDown[] = { -1, 1, 0, 0 };
+	static int leftRight[] = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int input[] = Arrays.stream(br.readLine().split(" ")).mapToInt(x -> Integer.valueOf(x)).toArray();
-		int height = input[0];
-		int width = input[1];
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
 
-		board = new char[height][width];
-		visited = new boolean[26];
-		max = 0;
-		for (int i = 0; i < height; i++) {
-			board[i] = br.readLine().toCharArray();
+		map = new char[R][C];
+
+		for (int i = 0; i < R; i++) {
+			char input[] = br.readLine().toCharArray();
+			for (int j = 0; j < C; j++) {
+				map[i][j] = input[j];
+			}
 		}
 
-		visited[board[0][0] - 'A'] = true;
-		dfs(new Coordinate(0, 0), 1);
+		moveAlpha(new Coordinate(0, 0), 1);
 
-		bw.write(String.valueOf(max));
-		bw.newLine();
+		bw.write(maxMove + "\n");
 		bw.flush();
-
-		br.close();
 		bw.close();
+		br.close();
 	}
 
-	public static void dfs(Coordinate curr, int sum) {
-		int upDown[] = { -1, 1, 0, 0 };
-		int leftRight[] = { 0, 0, -1, 1 };
-
-		max = Math.max(sum, max);
+	public static void moveAlpha(Coordinate curr, int length) { // 방문하지 않은 알파벳으로 이동하는 메소드
+		maxMove = Math.max(maxMove, length);
+		visited[map[curr.row][curr.col] - 'A'] = true;
 
 		for (int k = 0; k < upDown.length; k++) {
 			Coordinate next = new Coordinate(curr.row + upDown[k], curr.col + leftRight[k]);
-
-			if (next.row < 0 || next.col < 0 || next.row >= board.length || next.col >= board[0].length
-					|| visited[board[next.row][next.col] - 'A']) {
+			if (next.row < 0 || next.col < 0 || next.row >= map.length || next.col >= map[0].length) {
 				continue;
-			} else {
-				visited[board[next.row][next.col] - 'A'] = true;
-				dfs(next, sum + 1);
-				visited[board[next.row][next.col] - 'A'] = false;
 			}
+			if (visited[map[next.row][next.col] - 'A']) {
+				continue;
+			}
+			visited[map[next.row][next.col] - 'A'] = true;
+			moveAlpha(next, length + 1);
+			visited[map[next.row][next.col] - 'A'] = false;
 		}
 	}
 }
