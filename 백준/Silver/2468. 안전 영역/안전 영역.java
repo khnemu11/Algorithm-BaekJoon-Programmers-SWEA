ac5210d1a889b822488de7c5187f7f3bce8b7364
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-02-24 11:30:47 +0900

    [Silver I] Title: 안전 영역, Time: 316 ms, Memory: 61524 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -3,87 +3,72 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
+/*
+	풀이 알고리즘
+	섬의 높이 별로 물에 잠길때 변화가 생기므로 섬의 높이를 중복없이 저장하여 개수 만큼 dfs 반복
+*/
 public class Main {
+	static int upDown[] = { -1, 1, 0, 0 };
+	static int leftRight[] = { 0, 0, -1, 1 };
+	static boolean visited[][];
+	static int map[][];
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int size = Integer.valueOf(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
-		int area[][] = new int[size][size];
-		int max = 1;
-
-		HashSet<Integer> heightSet = new HashSet<>();
-
-		for (int i = 0; i < size; i++) {
+		map = new int[N][N];
+		TreeSet<Integer> heightSet = new TreeSet<>(); // 높이를 중복없이 낮은 순서로 정렬하기 위한 set
+		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < size; j++) {
-				int height = Integer.valueOf(st.nextToken());
-				heightSet.add(height);
-				area[i][j] = height;
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.valueOf(st.nextToken());
+				heightSet.add(map[i][j]);
 			}
 		}
-		Iterator<Integer> it = heightSet.iterator();
-		int upDown[] = { -1, 1, 0, 0 };
-		int leftRight[] = { 0, 0, -1, 1 };
 
-		while (it.hasNext()) {
-			int height = it.next();
-//			System.out.println("height : " + height);
-			if (height == 1) {
-				Math.max(max, 1);
-				continue;
-			}
+		int max = 1;
 
-			Queue<Coordinate> queue = new LinkedList<>();
-			boolean visited[][] = new boolean[size][size];
+		for (int height : heightSet) {
+			visited = new boolean[N][N];
+			int areaCnt = 0;
 
-			int count = 0;
-			for (int i = 0; i < size; i++) {
-				for (int j = 0; j < size; j++) {
-					if (visited[i][j] || area[i][j] < height) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (visited[i][j] || map[i][j] <= height) { // 방문 했거나 현재 높이보다 낮은 건물이라면 패스
 						continue;
 					}
 
-					queue.add(new Coordinate(i, j));
-
-					while (!queue.isEmpty()) {
-						Coordinate curr = queue.poll();
-						if (visited[curr.row][curr.col]) {
-							continue;
-						}
-						visited[curr.row][curr.col] = true;
-
-						for (int k = 0; k < 4; k++) {
-							int nextRow = curr.row + upDown[k];
-							int nextCol = curr.col + leftRight[k];
-							if (nextRow < 0 || nextRow >= size || nextCol < 0 || nextCol >= size
-									|| visited[nextRow][nextCol] || area[nextRow][nextCol] < height) {
-								continue;
-							}
-							visited[curr.row][curr.col] = true;
-							queue.add(new Coordinate(nextRow, nextCol));
-						}
-					}
-
-					count++;
-
+					moveTallerThanFloor(new Coordinate(i, j), height);
+					areaCnt++; // 영역 개수 증가
 				}
 			}
-//			System.out.println("count : " + count);
-			max = Math.max(max, count);
+			max = Math.max(areaCnt, max);
 		}
-		bw.write(String.valueOf(max));
-		bw.newLine();
+		bw.write(max + "\n");
 		bw.flush();
-		br.close();
 		bw.close();
+		br.close();
+	}
+
+	public static void moveTallerThanFloor(Coordinate curr, int floor) { // 홍수보다 높이가 높은 건물만 dfs로 탐색(현재 좌표, 홍수 높이)
+		visited[curr.row][curr.col] = true;
+
+		for (int k = 0; k < upDown.length; k++) {
+			Coordinate next = new Coordinate(curr.row + upDown[k], curr.col + leftRight[k]);
+			if (next.row < 0 || next.col < 0 || next.row >= map.length || next.col >= map[0].length) {
+				continue;
+			}
+			if (visited[next.row][next.col] || map[next.row][next.col] <= floor) {
+				continue;
+			}
+			moveTallerThanFloor(next, floor);
+		}
 	}
 }
 
