599c33f24e30b113e24af5932e84d9f958d7c675
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-02-27 15:14:44 +0900

    [Gold V] Title: 집합의 표현, Time: 1300 ms, Memory: 55796 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -3,15 +3,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/*
+	풀이 알고리즘
+	0 : a를 부모로 b를 삽입
+	1 : a와 b가 같은 집합인지 판단 (YES NO)
+	유니온-파인드를 이용해 집합 병합
+*/
 public class Main {
 	static int parents[];
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.valueOf(st.nextToken());
-		int M = Integer.valueOf(st.nextToken());
+		int N = Integer.valueOf(st.nextToken()); // 집합의 개수
+		int M = Integer.valueOf(st.nextToken()); // 연산의 개수
 
 		parents = new int[N + 1];
 
@@ -21,7 +26,6 @@ public class Main {
 
 		for (int m = 0; m < M; m++) {
 			st = new StringTokenizer(br.readLine());
-
 			int command = Integer.valueOf(st.nextToken());
 			int a = Integer.valueOf(st.nextToken());
 			int b = Integer.valueOf(st.nextToken());
@@ -29,29 +33,31 @@ public class Main {
 			if (command == 0) {
 				union(a, b);
 			} else {
-				if (getParent(a) == getParent(b)) {
-					System.out.println("YES");
-				} else {
+				if (getParent(a) != getParent(b)) {
 					System.out.println("NO");
+				} else {
+					System.out.println("YES");
 				}
 			}
-
 		}
+
 	}
 
 	public static int getParent(int child) {
-		if (child == parents[child]) {
+		if (parents[child] == child) {
 			return child;
-		} else {
-			parents[child] = getParent(parents[child]);
-			return parents[child];
 		}
+
+		parents[child] = getParent(parents[child]);
+
+		return parents[child];
+
 	}
 
 	public static void union(int a, int b) {
 		int pa = getParent(a);
 		int pb = getParent(b);
 
-		parents[pa] = pb;
+		parents[pb] = pa;
 	}
 }
