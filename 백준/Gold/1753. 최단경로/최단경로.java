d60043cbda74e06fa69616bc1745c7806ac0a5a7
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-03-02 11:29:03 +0900

    [Gold IV] Title: 최단경로, Time: 816 ms, Memory: 113240 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
@@ -7,87 +8,84 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+/*
+ * 
+ * 풀이 알고리즘
+    1) 우선순위 큐를 활용한 다익스트라 알고리즘을 활용해서 최단거리 구하고 모두 출력
+*/
+
 public class Main {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int vertex = Integer.valueOf(st.nextToken());
-		int edge = Integer.valueOf(st.nextToken());
+		int N = Integer.valueOf(st.nextToken());
+		int E = Integer.valueOf(st.nextToken());
 		int start = Integer.valueOf(br.readLine());
-		ArrayList<ArrayList<Vertex>> graph = new ArrayList<ArrayList<Vertex>>();
-		for (int i = 0; i < vertex; i++) {
+
+		ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+
+		for (int i = 0; i <= N; i++) {
 			graph.add(new ArrayList<>());
 		}
 
-		for (int i = 0; i < edge; i++) {
+		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.valueOf(st.nextToken());
 			int to = Integer.valueOf(st.nextToken());
-			int weight = Integer.valueOf(st.nextToken());
-			Vertex curr = new Vertex(to - 1, weight);
+			int cost = Integer.valueOf(st.nextToken());
 
-			graph.get(from - 1).add(curr);
+			graph.get(from).add(new Node(to, cost));
 		}
-//		System.out.println(graph.toString());
-		int cost[] = new int[vertex];
-		for (int i = 0; i < vertex; i++) {
-			if (i == start - 1) {
+
+		int path[] = new int[N + 1];
+
+		int INF = 100000000;
+		Arrays.fill(path, INF);
+		path[start] = 0;
+
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(start, 0));
+
+		while (!pq.isEmpty()) {
+			Node from = pq.poll();
+
+			if (from.cost > path[from.val]) {
 				continue;
 			}
-			cost[i] = Integer.MAX_VALUE;
-		}
 
-		PriorityQueue<Vertex> queue = new PriorityQueue<>();
-		queue.add(new Vertex(start - 1, 0));
-
-		while (!queue.isEmpty()) {
-			Vertex curr = queue.poll();
-//		 	System.out.println(curr + curr.toString());
-			for (int j = 0; j < graph.get(curr.index).size(); j++) {
-				long currCost = cost[curr.index] + (long) graph.get(curr.index).get(j).cost;
-				int index = graph.get(curr.index).get(j).index;
-//				System.out.println(cost[index] + " vs curr : " + currCost);
-				if ((long) cost[index] > currCost) {
-					cost[index] = (int) currCost;
-					queue.add(new Vertex(index, cost[index]));
+			for (Node to : graph.get(from.val)) {
+				if (path[to.val] > path[from.val] + to.cost) {
+					path[to.val] = path[from.val] + to.cost;
+					pq.add(new Node(to.val, path[to.val]));
 				}
 			}
-//			System.out.println(queue.toString());
-//			System.out.println(Arrays.toString(cost));
 		}
 
-		for (int i = 0; i < vertex; i++) {
-			if (cost[i] == Integer.MAX_VALUE) {
+		for (int i = 1; i < path.length; i++) {
+			if (path[i] == INF) {
 				bw.write("INF");
 			} else {
-				bw.write(String.valueOf(cost[i]));
+				bw.write("" + path[i]);
 			}
 			bw.newLine();
 		}
+
 		bw.flush();
-		bw.close();
-		br.close();
 	}
 }
 
-class Vertex implements Comparable<Vertex> {
-	int index;
+class Node implements Comparable<Node> {
+	int val;
 	int cost;
 
-	public Vertex(int index, int cost) {
-		this.index = index;
+	public Node(int val, int cost) {
+		this.val = val;
 		this.cost = cost;
 	}
 
 	@Override
-	public int compareTo(Vertex o) {
+	public int compareTo(Node o) {
 		return this.cost - o.cost;
 	}
-
-	@Override
-	public String toString() {
-		return "Vertex [index=" + index + ", cost=" + cost + "]";
-	}
-
 }
