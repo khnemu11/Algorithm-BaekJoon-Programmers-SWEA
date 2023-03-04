c9448a859c345093b2784b7d2d69f337bf1fdbc3
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-03-04 22:53:07 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.out.println("|\\_/|\n" + 
+				"|q p|   /}\n" + 
+				"( 0 )\"\"\"\\\n" + 
+				"|\"^\"`    |\n" + 
+				"||_/=\\\\__|");
+	}
+}
