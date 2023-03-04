334bd5ebbf6d30de17c418cae9343e179c319a23
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-03-04 22:49:33 +0900

    [Bronze V] Title: A×B, Time: 148 ms, Memory: 16188 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int num[] = Arrays.stream(br.readLine().split(" ")).mapToInt(x -> Integer.valueOf(x)).toArray();
+		bw.write((num[0] * num[1]) + "\n");
+		bw.flush(); // 결과 출력
+		bw.close();
+	}
+}
