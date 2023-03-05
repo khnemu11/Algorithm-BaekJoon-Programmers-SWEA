b330bedb5f53ccb434dc9d3ccd79ba446a61d8b2
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-03-06 00:06:29 +0900

    [Gold V] Title: 인구 이동, Time: 556 ms, Memory: 79416 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -4,117 +4,161 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
+/*
+ * 풀이 알고리즘
+ * 목표)
+ * 인구 이동이 일어나는 총 횟수
+ * 
+ * 
+ * N : 맵의 크기
+ * L : 인구 차이의 최솟값
+ * R : 인구 차이의 최대값
+ * 
+ * 인구 이동 : 현재 도시의 인구가 인접한 4방향의 도시의 인구의 차이 diff가 L<= diff <=R을 만족하는 경우 해당 방향의 국경선을 연다(노드가 연결된다.)
+ * 인구 이동을 끝나면 해당 도시의 모든 인구더하고 를 도시의 개수만큼 나눈다.
+   
+  구현 방법) 
+   유니온-파인드를 이용해 인구이동 조건을 만족하는 도시들의 부모를 같게한다.
+  부모가 같은 곳만 방문하여 (dfs)인구의 합을 구한 후 연합한 도시의 개수로 나누어 분배한다.
+  인구 이동이 더이상 없을 때 까지 반복하고 횟수를 출력
+ */
+
 public class Main {
-	public static void main(String[] args) throws IOException {
+	static int parents[];
+	static int map[][];
+	static int populations[];
+	static int childNum[];
+	static int L;
+	static int R;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int size = Integer.valueOf(st.nextToken());
-		int L = Integer.valueOf(st.nextToken());
-		int R = Integer.valueOf(st.nextToken());
+		int N = Integer.valueOf(st.nextToken());
+		L = Integer.valueOf(st.nextToken());
+		R = Integer.valueOf(st.nextToken());
+
+		map = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.valueOf(st.nextToken());
+			}
+		}
+
+		int time = 0;
+//		printArr();
+		while (true) {
+			parents = new int[N * N];
+			populations = new int[N * N];
+			childNum = new int[N * N];
+
+			for (int i = 0; i < parents.length; i++) {
+				parents[i] = i;
+			}
+
+			if (!hasPopulationMove()) {
+				break;
+			}
+//			System.out.println(Arrays.toString(parents));
+//			System.out.println(Arrays.toString(populations));
+//			System.out.println(Arrays.toString(childNum));
+//			System.out.println();
+
+			movePopulation();
+//			printArr();
+			time++;
+		}
+
+		bw.write(time + "\n");
+		bw.flush(); // 결과 출력
+		bw.close();
+	}
+
+	public static void printArr() {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	public static void movePopulation() {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				int parent = getParent(i * map.length + j);
+				populations[parent] += map[i][j];
+				childNum[parent]++;
+			}
+		}
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				int parent = getParent(i * map.length + j);
+				map[i][j] = populations[parent] / childNum[parent];
+			}
+		}
+	}
+
+	public static boolean hasPopulationMove() {
+		boolean isValid = false;
 		int upDown[] = { -1, 1, 0, 0 };
 		int leftRight[] = { 0, 0, -1, 1 };
 
-		int country[][] = new int[size][size];
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				int currCity = (map.length * i + j);
+				for (int k = 0; k < upDown.length; k++) {
+					Coordinate next = new Coordinate(i + upDown[k], j + leftRight[k]);
+					int nextCity = (map.length * next.row + next.col);
 
-		for (int i = 0; i < size; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < size; j++) {
-				country[i][j] = Integer.valueOf(st.nextToken());
-			}
-		}
-
-		int count = 0;
-
-		while (true) {
-			boolean change = false;
-			boolean visited[][] = new boolean[size][size];
-
-			if (change == true) {
-				break;
-			}
-
-			for (int i = 0; i < size; i++) {
-				for (int j = 0; j < size; j++) {
-					if (visited[i][j]) {
+					if (next.row < 0 || next.col < 0 || next.row >= map.length || next.col >= map[0].length) {
+						continue;
+					}
+					if (getParent(nextCity) == getParent(currCity)) {
 						continue;
 					}
 
-					Queue<Coordinate> queue = new LinkedList<>();
-					queue.add(new Coordinate(i, j));
-					Queue<Coordinate> union = new LinkedList<>();
+					int diff = Math.abs(map[next.row][next.col] - map[i][j]);
 
-					int sum = 0;
-
-					while (!queue.isEmpty()) {
-						int repeat = queue.size();
-
-						for (int k = 0; k < repeat; k++) {
-							Coordinate curr = queue.poll();
-							if (visited[curr.row][curr.col]) {
-								continue;
-							}
-							union.add(curr);
-							sum += country[curr.row][curr.col];
-//							System.out.println("search : " + curr.toString());
-							visited[curr.row][curr.col] = true;
-
-							for (int move = 0; move < 4; move++) {
-								int nextRow = upDown[move] + curr.row;
-								int nextCol = leftRight[move] + curr.col;
-
-								if (nextRow < 0 || nextRow >= size || nextCol < 0 || nextCol >= size
-										|| visited[nextRow][nextCol]) {
-									continue;
-								}
-
-								int differ = Math.abs(country[curr.row][curr.col] - country[nextRow][nextCol]);
-
-								if (differ < L || differ > R) {
-									continue;
-								}
-
-								queue.add(new Coordinate(nextRow, nextCol));
-							}
-						}
+					if (L > diff || diff > R) {
+						continue;
 					}
-//					System.out.println(union.toString());
-					if (union.size() > 1) {
-						change = true;
-						int polulation = sum / union.size();
+					isValid = true;
 
-						while (!union.isEmpty()) {
-							Coordinate curr = union.poll();
-							country[curr.row][curr.col] = polulation;
-
-						}
-//						System.out.println(Arrays.deepToString(country));
-					}
-
+					union(nextCity, currCity);
 				}
-
 			}
-
-//			System.out.println();
-			if (!change) {
-				break;
-			}
-			count++;
 		}
-//		System.out.println(Arrays.deepToString(country));
-		bw.write(String.valueOf(count));
-		bw.newLine();
-		bw.flush();
-		bw.close();
-		br.close();
+
+		return isValid;
 	}
 
+	public static int getParent(int child) {
+		if (parents[child] == child) {
+			return parents[child];
+		}
+		parents[child] = getParent(parents[child]);
+
+		return parents[child];
+	}
+
+	public static void union(int a, int b) {
+		int pa = getParent(a);
+		int pb = getParent(b);
+
+		if (pa < pb) {
+			parents[pb] = pa;
+		} else {
+			parents[pa] = pb;
+		}
+	}
 }
 
 class Coordinate {
@@ -125,10 +169,4 @@ class Coordinate {
 		this.row = row;
 		this.col = col;
 	}
-
-	@Override
-	public String toString() {
-		return "Coordinate [row=" + row + ", col=" + col + "]";
-	}
-
 }
