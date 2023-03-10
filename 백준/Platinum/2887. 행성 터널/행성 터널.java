290a51311b35d2d5a9fe07f49152c3dba0b15fa6
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-03-10 09:30:10 +0900

    Update 행성 터널.java
    풀이 추가

--- 백준/Platinum/2887. 행성 터널/행성 터널.java
+++ 백준/Platinum/2887. 행성 터널/행성 터널.java
@@ -1,138 +1,142 @@
+package defalut;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+/*
+	풀이 알고리즘
+
+ 	두 행성간의 거리는 두 좌표의 거리가 아닌 x좌표, y좌표 ,z좌표의 차이 중 최소값
+ 	
+ 	단수 좌표끼리 모든 거리를 구하면 N^N으로 시간복잡도가 위험함
+ 	
+ 	
+ 	각 좌표의 차이(두 좌표간 경로)별로 최소인 것을 우선적으로 MST생성 -> 각 차이별 정렬 필요
+ 	같은 경로를 다시가면 안됨 -> 크루스칼을 이용해 경로 방문 확인
+ 	
+ 	이를 이용하면 정렬이 3번 일어나므로 3N*logN이기 때문에 시간 내에 가능
+ 	
+ */
 public class Main {
 	static int parents[];
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int size = Integer.valueOf(br.readLine());
-		Coordinate[] coordinates = new Coordinate[size];
-		parents = new int[size];
-		for (int i = 0; i < size; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			coordinates[i] = new Coordinate(i, Integer.valueOf(st.nextToken()), Integer.valueOf(st.nextToken()),
-					Integer.valueOf(st.nextToken()));
-		}
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		for (int i = 0; i < parents.length; i++) {
+		int N = Integer.valueOf(br.readLine());
+
+		Coordinate coordinates[] = new Coordinate[N];
+		parents = new int[N];
+		for (int i = 0; i < N; i++) {
 			parents[i] = i;
+
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int x = Integer.valueOf(st.nextToken());
+			int y = Integer.valueOf(st.nextToken());
+			int z = Integer.valueOf(st.nextToken());
+
+			coordinates[i] = new Coordinate(i, x, y, z);
 		}
 
-		Coordinate[] xCoordinates = Arrays.copyOf(coordinates, coordinates.length);
-		Coordinate[] yCoordinates = Arrays.copyOf(coordinates, coordinates.length);
-		Coordinate[] zCoordinates = Arrays.copyOf(coordinates, coordinates.length);
+		PriorityQueue<Path> pQueue = new PriorityQueue<>();
 
-		Arrays.sort(xCoordinates, new Comparator<Coordinate>() {
-			@Override
-			public int compare(Coordinate o1, Coordinate o2) {
-				return o1.x - o2.x;
+		Arrays.sort(coordinates, (o1, o2) -> o1.x - o2.x); // x정렬
+
+		for (int i = 0; i < coordinates.length - 1; i++) {
+			pQueue.add(new Path(coordinates[i], coordinates[i + 1]));
+		}
+
+		Arrays.sort(coordinates, (o1, o2) -> o1.y - o2.y);	// y정렬
+
+		for (int i = 0; i < coordinates.length - 1; i++) {
+			pQueue.add(new Path(coordinates[i], coordinates[i + 1]));
+		}
+		Arrays.sort(coordinates, (o1, o2) -> o1.z - o2.z);	// z정렬
+
+		for (int i = 0; i < coordinates.length - 1; i++) {
+			pQueue.add(new Path(coordinates[i], coordinates[i + 1]));
+		}
+
+		int cost = 0;	//최소 비용 합
+
+		while (!pQueue.isEmpty()) {
+			Path minPath = pQueue.poll();
+
+			if (doUnion(minPath)) {	//만약 사이클이 형셩되는 경로가 아니라면 비용 추가
+				cost += minPath.cost;	
 			}
-
-		});
-		Arrays.sort(yCoordinates, new Comparator<Coordinate>() {
-			@Override
-			public int compare(Coordinate o1, Coordinate o2) {
-				return o1.y - o2.y;
-			}
-
-		});
-		Arrays.sort(zCoordinates, new Comparator<Coordinate>() {
-			@Override
-			public int compare(Coordinate o1, Coordinate o2) {
-				return o1.z - o2.z;
-			}
-		});
-
-		PriorityQueue<Vertex> pq = new PriorityQueue<>();
-
-		for (int i = 0; i < xCoordinates.length - 1; i++) {
-			pq.add(new Vertex(xCoordinates[i], xCoordinates[i + 1]));
-		}
-		for (int i = 0; i < yCoordinates.length - 1; i++) {
-			pq.add(new Vertex(yCoordinates[i], yCoordinates[i + 1]));
-		}
-		for (int i = 0; i < zCoordinates.length - 1; i++) {
-			pq.add(new Vertex(zCoordinates[i], zCoordinates[i + 1]));
 		}
 
-		int cnt = 0;
-		long sum = 0;
-		while (cnt < size - 1) {
-			Vertex curr = pq.poll();
-
-			if (getParent(curr.start.val) == getParent(curr.end.val)) {
-				continue;
-			}
-
-			union(curr.start.val, curr.end.val);
-			cnt++;
-			sum += curr.min;
-		}
-
-		System.out.println(sum);
+		bw.write(cost + "\n");
+		bw.flush();
 	}
 
 	public static int getParent(int child) {
-		if (child == parents[child]) {
-			return child;
-		} else {
-			parents[child] = getParent(parents[child]);
+		if (parents[child] == child) {
 			return parents[child];
 		}
+
+		parents[child] = getParent(parents[child]);
+
+		return parents[child];
 	}
 
-	public static void union(int a, int b) {
-		int pa = getParent(a);
-		int pb = getParent(b);
+	public static boolean doUnion(Path path) {
+		int pa = getParent(path.startCoordinate.seq);
+		int pb = getParent(path.endCoordinate.seq);
 
-		parents[pa] = pb;
-	}
-}
+		if (pa == pb) {
+			return false;
+		}
 
-class Vertex implements Comparable<Vertex> {
-	Coordinate start;
-	Coordinate end;
-	int min;
+		else if (pa < pb) {
+			parents[pb] = pa;
+		} else {
+			parents[pa] = pb;
+		}
 
-	public Vertex(Coordinate start, Coordinate end) {
-		this.start = start;
-		this.end = end;
-		min = Math.min(Math.abs(start.x - end.x), Math.abs(start.y - end.y));
-		min = Math.min(min, Math.abs(start.z - end.z));
-	}
-
-	@Override
-	public int compareTo(Vertex o) {
-		return this.min - o.min;
-	}
-
-	@Override
-	public String toString() {
-		return "Vertex [start=" + start + ", end=" + end + ", min=" + min + "]";
+		return true;
 	}
 }
 
 class Coordinate {
-	int val;
+	int seq;
 	int x;
 	int y;
 	int z;
 
-	public Coordinate(int val, int x, int y, int z) {
-		this.val = val;
+	public Coordinate(int seq, int x, int y, int z) {
+		this.seq = seq;
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
+}
+
+class Path implements Comparable<Path> {
+	Coordinate startCoordinate;
+	Coordinate endCoordinate;
+	int cost;
+
+	public Path(Coordinate startCoordinate, Coordinate endCoordinate) {
+		this.startCoordinate = startCoordinate;
+		this.endCoordinate = endCoordinate;
+
+		this.cost = Math.min(Math.abs(startCoordinate.x - endCoordinate.x),
+				Math.abs(startCoordinate.y - endCoordinate.y));
+		this.cost = Math.min(this.cost, Math.abs(startCoordinate.z - endCoordinate.z));
+	}
 
 	@Override
-	public String toString() {
-		return "Coordinate [val=" + val + ", x=" + x + ", y=" + y + ", z=" + z + "]";
+	public int compareTo(Path o) {
+		return this.cost - o.cost;
 	}
 }
