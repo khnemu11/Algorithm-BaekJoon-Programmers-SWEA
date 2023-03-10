296e6c5c70145835904ccf7cf3a921c83a7b6e9f
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-03-10 13:47:56 +0900

    Update 두 큐 합 같게 만들기.java
    풀이과정 추가

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,5 +1,22 @@
 import java.util.*;
 
+/*
+    풀이 알고리즘
+    
+    입력된 큐를 큐의 알고리즘에 맞게 숫자를 빼내어 서로 다른 2개의 큐 안의 수의 합이 같을 때 몇번 꺼내야 하는지 리턴
+    
+    서로 다른 2개의 큐 안의 수의 합이 같다 == (모든 큐의 수의 합 )/2
+    
+    만약 한쪽의 큐가 목표 값((모든 큐의 수의 합 )/2)보다 작으면 다른쪽큐의 값을 꺼내서 현재 큐에 삽입
+    만약 한쪽의 큐가 목표 값((모든 큐의 수의 합 )/2)보다 크면 현재 큐의 값을 꺼내서 다른쪽 큐로 삽입
+    같으면 횟수 출력
+    
+    이때 같은 숫자가 2번(한쪽 큐, 다른쪽 큐)까지 나타날 수 있고 3번이상 나오면 루프를 돌고 있다는 뜻
+    -> 정답은 없다.
+    
+    걸린시간 : 25분
+*/
+
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
         int answer = -2;
@@ -24,9 +41,6 @@ class Solution {
         boolean isFind =false;
 
         while(true){
-            // System.out.println(sum[1]+" "+sum[2]);
-            // System.out.println(q1+" \n"+q2);
-            
             if(sum[1]==sum[2]){
                 isFind=true;
                 break;
