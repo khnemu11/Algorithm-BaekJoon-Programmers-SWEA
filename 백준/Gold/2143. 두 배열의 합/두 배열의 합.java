2c982ed11659c9f000ab781a53033dc7ee0efac2
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-03-10 11:36:23 +0900

    [Gold III] Title: 두 배열의 합, Time: 664 ms, Memory: 116604 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -1,86 +1,65 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashMap;
 
+/*
+	풀이 알고리즘
+	
+	a의 모든 누적합 경우의 수와 b의 모든 누적합 경우의 수를 구한다
+	-> n*n/2 *2 = n*n -> 1_000_000
+	
+	각각 hashmap으로 (누적합 값 : 개수)로 저장 최대 
+	
+	누적합의 최대값 = 1,000,000 * 1,000 > 20억
+	개수의 최대값 = 1,000,000 * 10,000,000 > 20억
+	long형으로 선언
+ */
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static boolean airArea[][];
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.valueOf(br.readLine());
-		int aLength = Integer.valueOf(br.readLine());
-		int a[] = new int[aLength];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < a.length; i++) {
-			a[i] = Integer.valueOf(st.nextToken());
-		}
-		ArrayList<Integer> aSumList = new ArrayList<>();
-		for (int i = 0; i < a.length; i++) {
-			int sum = 0;
-			for (int j = i; j < a.length; j++) {
-				sum += a[j];
-				aSumList.add(sum);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		long target = Long.valueOf(br.readLine());
+
+		int aSize = Integer.valueOf(br.readLine()); // A의 모든 누적합을 누하고 해당 누적합을 (누적합 : 누적합의 개수)의 맵에 저장하는 부분
+		int numA[] = Arrays.stream(br.readLine().split(" ")).mapToInt(x -> Integer.valueOf(x)).toArray();
+		HashMap<Long, Long> aMap = new HashMap<>();
+		for (int i = 0; i < numA.length; i++) {
+			long sum = 0;
+			for (int j = i; j < numA.length; j++) {
+				sum += numA[j];
+				aMap.put(sum, aMap.getOrDefault(sum, 0L) + 1);
 			}
 		}
-		Collections.sort(aSumList);
 
-		int bLength = Integer.valueOf(br.readLine());
-		int b[] = new int[bLength];
+		int bSize = Integer.valueOf(br.readLine());// B의 모든 누적합을 누하고 해당 누적합을 (누적합 : 누적합의 개수)의 맵에 저장하는 부분
+		int numB[] = Arrays.stream(br.readLine().split(" ")).mapToInt(x -> Integer.valueOf(x)).toArray();
+		HashMap<Long, Long> bMap = new HashMap<>();
 
-		st = new StringTokenizer(br.readLine());
-
-		for (int i = 0; i < b.length; i++) {
-			b[i] = Integer.valueOf(st.nextToken());
-		}
-
-		ArrayList<Integer> bSumList = new ArrayList<>();
-		for (int i = 0; i < b.length; i++) {
-			int sum = 0;
-			for (int j = i; j < b.length; j++) {
-				sum += b[j];
-				bSumList.add(sum);
+		for (int i = 0; i < numB.length; i++) {
+			long sum = 0;
+			for (int j = i; j < numB.length; j++) {
+				sum += numB[j];
+				bMap.put(sum, bMap.getOrDefault(sum, 0L) + 1);
 			}
 		}
-		Collections.sort(bSumList);
 
-		long cnt = 0;
-		int aIdx = 0;
-		int bIdx = bSumList.size() - 1;
+		long cnt = 0; // 총 부 배열 쌍의 개수
 
-		while (aIdx < aSumList.size() && bIdx >= 0) {
-			long sum = aSumList.get(aIdx) + bSumList.get(bIdx);
-			if (sum == T) {
-
-				int currA = aSumList.get(aIdx);
-				int currB = bSumList.get(bIdx);
-				long aCnt = 0;
-				long bCnt = 0;
-				while (true) {
-					if (aIdx < aSumList.size() && currA == aSumList.get(aIdx)) {
-						aCnt++;
-						aIdx++;
-					} else {
-						break;
-					}
-				}
-
-				while (true) {
-					if (bIdx >= 0 && currB == bSumList.get(bIdx)) {
-						bCnt++;
-						bIdx--;
-					} else {
-						break;
-					}
-				}
-				cnt += aCnt * bCnt;
-			} else if (sum > T) {
-				bIdx--;
-
-			} else {
-				aIdx++;
+		for (long valA : aMap.keySet()) { // a의 누적합을 하나씩 꺼내기
+			if (bMap.get(target - valA) == null) { // 목표값 - a의 누적합이 b에 없으면 a로는 목표값 생성 불가
+				continue;
 			}
+			cnt = cnt + aMap.get(valA) * bMap.get(target - valA); // a의 누적합 개수와 b의 누적합 개수를 곱하는 것이 a와 b의 조합의 수
 		}
-		System.out.println(cnt);
+
+		bw.write(cnt + "\n");
+		bw.flush();
 	}
 }
