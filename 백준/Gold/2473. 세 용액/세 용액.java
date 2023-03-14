fdf9ea67dcdcd125197cfcb2bd1a03a2cd5f5448
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-03-14 13:01:57 +0900

    [Gold III] Title: 세 용액, Time: 260 ms, Memory: 17980 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -6,51 +6,46 @@ import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 /*
- * 풀이 과정
- * 
- * 2개의 용액은 이분탐색으로 탐색 가능하므로 1개의 용액을 기준으로 모든 오른쪽의 용액을 이분탐색을 해서 최소값을 구한다
- * 시간 복잡도는 n*logn으로 시간 내에 가능
- * 
- * 
- * */
-
+	풀이 알고리즘
+	
+	정렬 후 용액 2개를 골라 투포인터 탐색
+	n*n = O(n^2)
+ */
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.valueOf(br.readLine());
-		int nums[] = Arrays.stream(br.readLine().split(" ")).mapToInt(x -> Integer.valueOf(x)).toArray();
+		long nums[] = Arrays.stream(br.readLine().split(" ")).mapToLong(x -> Long.valueOf(x)).toArray();
+
 		Arrays.sort(nums);
-		long min = Math.abs((long)nums[0] + nums[1] + nums[2]);
-		int result[] = { nums[0], nums[1], nums[2] };
-		for (int i = 0; i < nums.length - 2; i++) {
-			int start = i;
-			int l = start + 1;
+
+		long min = Long.MAX_VALUE;
+		long result[] = { nums[0], nums[1], nums[2] };
+
+		for (int i = 0; i < nums.length; i++) {
+			long num = nums[i];
+
+			int l = i + 1;
 			int r = nums.length - 1;
 
 			while (l < r) {
-				if (Math.abs((long) nums[start] + nums[l] + nums[r]) < min) {
-					min = Math.abs((long) nums[start] + nums[l] + nums[r]);
-					result[0] = nums[start];
+				long sum = num + nums[l] + nums[r];
+				if (Math.abs(sum) < Math.abs(min)) {
+					min = sum;
+					result[0] = num;
 					result[1] = nums[l];
 					result[2] = nums[r];
-					if (min == 0) {
-						break;
-					}
 				}
-
-				if ((long) nums[start] + nums[l] + nums[r] > 0) {
-					r--;
-				} else {
+				if (sum < 0) {
 					l++;
+				} else {
+					r--;
 				}
-			}
-
-			if (min == 0) {
-				break;
 			}
 		}
+
 		bw.write(result[0] + " " + result[1] + " " + result[2] + "\n");
 		bw.flush();
 	}
