a69c2dfdbe7054425359a21ef725ab3825878645
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-03-30 09:49:51 +0900

    Update 앱.java
    풀이과정 수정

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -3,54 +3,72 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/*
+/* 
+ * 	풀이 알고리즘
  * 
- * 풀이 알고리즘
-    1) 냅색을 위한 행 : 어플, 열 : 메모리 or 비용
-    2) 메모리를 열로 사용하기에는 값이 너무 크므로 비용을 열로 사용
-    3) 해당 비용 별 최대의 메모리 값을 dp를 이용해 저장
-    4) 가장 작은 비용부터 비용 별 메모리값을 순회하여 만약 목표 메모리보다 크거나 같다면 가장 적은 비용이므로 출력 후 루프 탈출
-*/
+ *  정해진 크기 이내,이상의 최대 값 = 냅색 알고리즘
+ * 	
+ * 	메모리 크기로 가방의 크기로 한다
+ *  -> 메모리의 크기는 최대 합은 10,000,000 * 100
+ *  O(n) = 핸드폰의 개수 * 최대 메모리의 합 =100*10,000,000 * 100 > 1억 
+ *  시간 초과
+ *  
+ *  비활성화 비용의 합을 가방의 크기로 한다.
+ *  -> 비활성화 비용의 최대 합은 100*100
+ *  O(n) = 핸드폰의 개수 * 최대 비활성화 비용의 합 =100*100 * 100 <1억 
+ *  시간 내 가능
+ *  
+ *  비활성화 크기 별 얻을 수 있는 최대 메모리의 크기를 냅색 알고리즘으로 구한 뒤
+ *  목표 메모리크기보다 같거나 크고 가장 작은 비활성화 크기를 출력 
+ * 
+ * */
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.valueOf(st.nextToken());
-		int target = Integer.valueOf(st.nextToken());
-		App apps[] = new App[N + 1];
 
-		StringTokenizer sizeSt = new StringTokenizer(br.readLine());
-		StringTokenizer costSt = new StringTokenizer(br.readLine());
-		int costSum = 0;
+		int N = Integer.valueOf(st.nextToken()); // 물건 개수
+		int M = Integer.valueOf(st.nextToken()); // 비활성화 어플 크기
 
-		for (int i = 1; i <= N; i++) {
-			int size = Integer.valueOf(sizeSt.nextToken());
-			int cost = Integer.valueOf(costSt.nextToken());
+		App apps[] = new App[N + 1]; // 탐색할 어플배열
 
-			apps[i] = new App(size, cost);
-			costSum += cost;
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 1; i <= N; i++) { // 어플의 메모리 값 설정
+			apps[i] = new App();
+			apps[i].memory = Integer.valueOf(st.nextToken());
 		}
-		int dp[] = new int[costSum + 1];
-		Arrays.fill(dp, -1);
-		dp[0] = 0;
+		st = new StringTokenizer(br.readLine());
 
-		for (int i = 1; i < apps.length; i++) {
-			for (int j = dp.length - 1; j >= apps[i].cost; j--) {
-				if (dp[j - apps[i].cost] != -1) {
-					dp[j] = Math.max(dp[j], dp[j - apps[i].cost] + apps[i].size);
+		int costSum = 0; // 비활성화 비용의 합
+
+		for (int i = 1; i <= N; i++) {// 어플의 비활성화 비용 설정
+			apps[i].cost = Integer.valueOf(st.nextToken());
+			costSum += apps[i].cost;
+		}
+
+		int maxMemeory[][] = new int[N + 1][costSum + 1]; // 비활성화 비용의 합을 가방의 크기로 하여 비활성화 비용 별 얻을 수 있는 최대 메모리 크기를 나타내는 dp
+															// 배열 생성
+
+		for (int i = 1; i <= N; i++) { // 냅색 알고리즘
+			for (int j = 0; j <= costSum; j++) {
+				if (j < apps[i].cost) {
+					maxMemeory[i][j] = maxMemeory[i - 1][j];
+				} else {
+					maxMemeory[i][j] = Math.max(maxMemeory[i - 1][j],
+							maxMemeory[i - 1][j - apps[i].cost] + apps[i].memory);
 				}
 			}
 		}
 
-		for (int i = 0; i < dp.length; i++) {
-			if (dp[i] >= target) {
-				bw.write("" + i);
+		for (int i = 0; i <= costSum; i++) { // 목표 메모리보다 같거나 큰 메모리 중 가장 작은 비활성화 비용을 가진 비용 탐색 및 출력
+			if (maxMemeory[N][i] >= M) {
+				bw.write(i + "\n");
 				break;
 			}
 		}
@@ -60,16 +78,9 @@ public class Main {
 }
 
 class App {
-	int size;
+	int memory;
 	int cost;
 
-	public App(int size, int cost) {
-		this.size = size;
-		this.cost = cost;
-	}
-
-	@Override
-	public String toString() {
-		return "App [size=" + size + ", cost=" + cost + "]";
+	public App() {
 	}
 }
