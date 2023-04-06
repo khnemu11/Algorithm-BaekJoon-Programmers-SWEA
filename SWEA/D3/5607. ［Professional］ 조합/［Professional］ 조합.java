055128cd413ebbb7964230ba86fa91a597a0ce60
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-04-06 12:37:36 +0900

    [D3] Title: [Professional] 조합, Time: 766 ms, Memory: 80,044 KB -BaekjoonHub

--- SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
+++ SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
@@ -1,55 +1,64 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-import java.util.Scanner;
+/*
+ * 풀이 알고리즘
+ * 
+ * 큰 이항정리 값을 구하는 알고리즘 -> 페르마소정리 + 분할정복을 이용한 거듭제곱
+ * 
+ * */
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static long MODULAR = 1234567891;
 
-		int T = sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		for (int test_case = 1; test_case <= T; test_case++) {
-			int L = sc.nextInt();
-			int R = sc.nextInt();
-			long LFactorial = 1;
-			long RFactorial = 1;
-			long LMinusRFactorial = 1;
+		int T = Integer.valueOf(br.readLine());
 
-			for (int i = 1; i <= L; i++) {
-				LFactorial = LFactorial * i % 1234567891;
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 
-				if (i <= R) {
-					RFactorial = RFactorial * i % 1234567891;
-				}
-				if (i <= L - R) {
-					LMinusRFactorial = LMinusRFactorial * i % 1234567891;
-				}
+			int N = Integer.valueOf(st.nextToken());
+			int R = Integer.valueOf(st.nextToken());
 
+			long factorial[] = new long[N + 1];
+			factorial[1] = 1;
+			//팩토리얼을 구하는 부분
+			for (int i = 2; i <= N; i++) {
+				factorial[i] = ((factorial[i - 1] % MODULAR) * (i % MODULAR)) % MODULAR;
 			}
-			long result = LFactorial * pow(((RFactorial * LMinusRFactorial) % 1234567891), 1234567891 - 2) % 1234567891;
+			//분모를 구하는 부분 N * (N-R)
+			long denominator = ((factorial[R] % MODULAR) * (factorial[N - R] % MODULAR)) % MODULAR;
+			//페르마 소정리를 이용해서 이항 정리를 구하는 부분
+       
+			long result = ((factorial[N] % MODULAR) * (pow(denominator, MODULAR - 2))) % MODULAR;
 
-			StringBuilder sb = new StringBuilder();
-			sb.append("#").append(test_case).append(" ");
-			sb.append(result);
-
-			System.out.println(sb.toString());
-
+			System.out.println("#" + tc + " " + result);
 		}
-
 	}
-
-	public static long pow(long base, int ex) {
-		if (ex == 0) {
+	//거듭 제곱을 분할 정복으로 구하는 메소드
+	public static long pow(long n, long exp) {
+		if (exp == 0) {
 			return 1;
-		} else if (ex == 1) {
-			return base;
+		} else if (exp == 1) {
+			return n;
+		}
+		long result = 1L;
+
+		long half = pow(n, exp / 2);
+
+		result = ((half % MODULAR) * (half % MODULAR)) % MODULAR;
+
+		if (exp % 2 == 1) {
+			result = ((result % MODULAR) * (n % MODULAR)) % MODULAR;
 		}
 
-		long half = pow(base, ex / 2);
-
-		if (ex % 2 == 0) {
-			return ((half % 1234567891) * (half % 1234567891)) % 1234567891;
-		} else {
-			return ((half % 1234567891) * (half % 1234567891) % 1234567891) * base % 1234567891;
-		}
+		return result;
 	}
 }
