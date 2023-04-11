13eb5d6f9422f4d44e93a0a79e48fe8bed3ae236
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-04-11 13:12:27 +0900

    Update 전화번호 목록.java

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,5 +1,12 @@
 import java.util.*;
 
+/*
+    접두어 표현을 위해 트라이 사용
+    트라이로 내려가면서 만약 중간에 끝이 나거나 끝이 났는대도 다음이 있다면 접두사가 있다
+    
+*/
+
+
 class Solution {
     static boolean prefix;
     
@@ -22,7 +29,7 @@ class Solution {
     public void insert(String word, Trie trieNode){
         for(int i=0;i<word.length();i++){
             Trie next = new Trie();
-            
+            //현재 매칭하는 문자열을 접두사로 쓰이는 경우
             if(trieNode.isLast){
                 prefix=false;
             }
@@ -35,7 +42,7 @@ class Solution {
             
             trieNode = next;
         }
-        
+        //다음 문자열도 있어서 현재 문자열이 접두사가 되는 경우
         if(trieNode.child.size()>0){
             prefix=false;
         }
