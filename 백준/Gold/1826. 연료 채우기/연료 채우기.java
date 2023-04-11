874eca40bee8f8cd92c9086d923978ee42fddb2f
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-04-11 11:17:55 +0900

    [Gold II] Title: 연료 채우기, Time: 256 ms, Memory: 20316 KB -BaekjoonHub

--- 백준/Gold/1826. 연료 채우기/연료 채우기.java
+++ 백준/Gold/1826. 연료 채우기/연료 채우기.java
@@ -7,8 +7,8 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 /*
- * 현재 내가 갈 수 있는 주유소 중 가장 많이 기름을 주는 주유소 선택
  * 
+ * 현재 내가 갈 수 있는 주유소 중 가장 많이 기름을 주는 주유소 선택
  * 
  * */
 
@@ -18,8 +18,12 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.valueOf(br.readLine());
+		
+		//주유소를 거리가 가까운 순서로 정렬한 큐
 		PriorityQueue<GasStation> stationByDistance = new PriorityQueue<>(N, (o1, o2) -> o1.distance - o2.distance);
+		//주유소를 가스 양을 많이 주는 순서로 정렬한 큐
 		PriorityQueue<GasStation> stationByAmount = new PriorityQueue<>(N, (o1, o2) -> o2.amount - o1.amount);
+		
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int distance = Integer.valueOf(st.nextToken());
@@ -29,29 +33,36 @@ public class Main {
 		}
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int villege = Integer.valueOf(st.nextToken());
-		int currentGas = Integer.valueOf(st.nextToken());
-		int location = 0;
-		int cnt = 0;
-		while (!stationByDistance.isEmpty()) {
-			if (location + currentGas >= villege) {
+		
+		int villege = Integer.valueOf(st.nextToken()); //마을까지의 거리
+		int currentGas = Integer.valueOf(st.nextToken()); //현재 가스 양
+		int location = 0;	//현재 위치
+		int cnt = 0;	//주유소를 들린 횟수
+		
+		while (!stationByDistance.isEmpty()) {	//다음 거리별 주유소 후보가 없을 때 까지
+			if (location + currentGas >= villege) {	//현재 위치의 가스양으로 마을로 갈 수 있을 때
 				break;
 			}
-
+			
+			//현재 가스양으로 갈 수 있는 주유소를 후보지로 등록
 			while (!stationByDistance.isEmpty() && location + currentGas >= stationByDistance.peek().distance) {
 				stationByAmount.add(stationByDistance.poll());
 			}
-
+			
+			///만약 후보지가 있으면 가장 가스양이 높은 주유소로 이동
 			if (!stationByAmount.isEmpty()) {
 				GasStation next = stationByAmount.poll();
 				currentGas = currentGas - (next.distance - location) + next.amount;
 				location = next.distance;
 				cnt++;
-			} else {
+			} 
+			//없다면 더이상 갈 수 있는 주유소가 없으므로 반복문 탈출
+			else {
 				break;
 			}
 		}
-
+		
+		//현재 위치에서 마을로 갈 수 없으면 -1 출력
 		if (location + currentGas < villege) {
 			bw.write("-1\n");
 		} else {
@@ -72,10 +83,4 @@ class GasStation {
 		this.amount = amount;
 		this.distance = distance;
 	}
-
-	@Override
-	public String toString() {
-		return "GasStation [amount=" + amount + ", distance=" + distance + "]";
-	}
-
 }
