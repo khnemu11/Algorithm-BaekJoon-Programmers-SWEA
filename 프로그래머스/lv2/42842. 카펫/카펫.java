79bd615b8b3507b3b71e894bbeef4d3eca556b5e
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-04-13 16:06:53 +0900

    Update 카펫.java

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,5 +1,14 @@
 import java.util.*;
 
+/*
+    갈색 영역의 너비 = (전체 넓이의 가로 - 2(끝테두리))*2(2개) + (전체 넓이의 세로 - 2(끝테두리))*2(2개) + 4개(양 끝 테두리)
+    if 갈색 영역의 너비 == 주어진 brown값 ==> 카펫의 너비
+    
+    카펫의 너비는 최대 sqrt(전체 카펫의 넓이) 까지 된다
+
+    시간 복잡도 = O(sqrt(brown + yellow))
+*/
+
 class Solution {
     public int[] solution(int brown, int yellow) {
         int total = brown + yellow;
