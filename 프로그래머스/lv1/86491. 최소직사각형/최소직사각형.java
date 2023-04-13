6fd894ace7d5368f9aada0e250ee3adf1b8ce1f7
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-04-13 14:58:59 +0900

    [level 1] Title: 최소직사각형, Time: 1.51 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -6,46 +6,10 @@ class Solution {
         int heightMin = 0;
         
         for(int i=0;i<sizes.length;i++){
-           widthMin = Math.max(sizes[i][0],widthMin);
-           heightMin = Math.max(heightMin,sizes[i][1]);
+           widthMin = Math.max(widthMin,Math.max(sizes[i][0],sizes[i][1]));
+           heightMin = Math.max(heightMin,Math.min(sizes[i][0],sizes[i][1]));
         }
-        
-        // System.out.println("start: "+widthMin+"*"+heightMin);
-        
-        for(int i=0;i<sizes.length;i++){
-            int height = getHeight(sizes[i][0], sizes,i);
-            // System.out.println(sizes[i][0]+"*"+height);
-            if(height != Integer.MAX_VALUE && widthMin * heightMin > height * sizes[i][0]){
-                widthMin = sizes[i][0];
-                heightMin = height;
-            }
-            
-            height = getHeight(sizes[i][1], sizes,i);
-            // System.out.println(sizes[i][1]+"*"+height);
-            if(height != Integer.MAX_VALUE && widthMin * heightMin > height * sizes[i][1]){
-                widthMin = sizes[i][1];
-                heightMin = height;
-            }
-        }
-        
-        // System.out.println("result " +widthMin+"*"+heightMin);
         
         return widthMin*heightMin;
-    }
-    public int getHeight(int width, int [][]sizes,int idx){
-        int height = 0;
-        
-        for(int i=0;i<sizes.length;i++){
-            if(sizes[i][0] > width && sizes[i][1] > width){
-                 return Integer.MAX_VALUE;
-            }else if(sizes[i][0]<=width && sizes[i][1]<=width){
-                height = Math.max(height,Math.min(sizes[i][1],sizes[i][0]));
-            }else if(sizes[i][0]<=width){
-                height = Math.max(height,sizes[i][1]);
-            }else{
-                height = Math.max(height,sizes[i][0]);
-            }
-        }
-        return height;
     }
 }
