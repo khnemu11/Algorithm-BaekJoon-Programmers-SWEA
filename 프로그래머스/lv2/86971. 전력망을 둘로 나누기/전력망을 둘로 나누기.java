487b7c7e9b8d19a16e62d28e8fd2f240caf28674
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-04-13 16:33:27 +0900

    Update 전력망을 둘로 나누기.java

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,7 +3,6 @@
     전선(경로)를 하나씩 골라 해당 전선을 제외하고 그래프 생성
     각 경로를 유니온 파인드로 부모 노드에 연결
     루트 부모별 개수의 차이중 최소값을 저장
-    
 */
 
 import java.util.*;
@@ -15,27 +14,30 @@ class Solution {
     public int solution(int n, int[][] wires) {
         minDiff = n;
         
+        //끊을 전선을 고르는 부분
         for(int i=0;i<wires.length;i++){
             makeTop(n,i,wires);
         }
         
         return minDiff;
     }
-    
+    //전력망 네트워크를 만드는 부분
     public void makeTop(int n, int exceptIdx,int[][] wires){
+        //부모 배열 초기화
         parents = new int[n+1];
         for(int i=0;i<=n;i++){
             parents[i] = i;
         }
         
+        //제외할 전선을 빼고 모든 전선끼리 병합
         for(int i=0;i<wires.length;i++){
-            if(i == exceptIdx){
+            if(i == exceptIdx){ 
                 continue;
             }
             
             union(wires[i][0],wires[i][1]);
         }
-        
+        //루트별 전선의 개수를 가지고 있는 해쉬맵
         HashMap<Integer,Integer> topCnt = new HashMap<>();
         
         for(int i=1;i<=n;i++){
@@ -43,6 +45,7 @@ class Solution {
             topCnt.put(parent,topCnt.getOrDefault(parent,0)+1);
         }
         
+        //네트워크별 전선개수의 차이를 구하는 부분
         int diff =0;
         
         for(int key : topCnt.keySet()){
@@ -53,9 +56,8 @@ class Solution {
             }
         }
         minDiff = Math.min(Math.abs(diff),minDiff);
-        // System.out.println(topCnt);
     }
-    
+    //부모값을 구하는 부분
     public int getParent(int child){
         if(parents[child] == child){
             return parents[child];
@@ -64,7 +66,7 @@ class Solution {
         parents[child] = getParent(parents[child]);
         return parents[child];
     }
-    
+    //네트워크의 병합 메소드
     public void union(int a, int b){
         int pa = getParent(a);
         int pb = getParent(b);
