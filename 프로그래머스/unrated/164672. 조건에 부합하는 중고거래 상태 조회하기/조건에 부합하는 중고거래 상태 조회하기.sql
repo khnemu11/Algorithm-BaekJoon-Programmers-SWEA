15e2589da540e8f0a88db2973eb802b888aa1d3f
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-04-17 11:11:49 +0900

    [unrated] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE,
+case
+    when status = 'DONE'
+    then '거래완료'
+    when status = 'SALE'
+    then '판매중'
+    else '예약중'      
+end
+as STATUS from USED_GOODS_BOARD
+where CREATED_DATE = '2022-10-05'
+order by BOARD_ID desc;
