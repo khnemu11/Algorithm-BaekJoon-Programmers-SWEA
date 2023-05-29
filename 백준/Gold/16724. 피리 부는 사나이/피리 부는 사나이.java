6b2c0ad978efc3be6c0003dfd92116a75fb472b3
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-05-29 17:03:38 +0900

    Update 피리 부는 사나이.java
    풀이 과정 추가

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
@@ -7,7 +7,7 @@ import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int parents[];
+	static int parents[];	//분리 집합을 위한 부모 배열
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,19 +17,22 @@ public class Main {
 		int height = Integer.valueOf(st.nextToken());
 		int width = Integer.valueOf(st.nextToken());
 		parents = new int[width * height];
-
+		
+		//부모 배열 초기화
 		for (int i = 0; i < parents.length; i++) {
 			parents[i] = i;
 		}
-
+		
+		//그래프 탐색
 		for (int row = 0; row < height; row++) {
 			String input = br.readLine();
 
 			for (int col = 0; col < input.length(); col++) {
-				char direction = input.charAt(col);
-				int parent = row * width + col;
-				int child = 0;
-
+				char direction = input.charAt(col); // 현재 위치
+				int parent = row * width + col;	//2차원 맵을 1차원으로 했을 때 나오는 현재 인덱스
+				int child = 0;	//다음 방향의 인덱스
+				
+				//상하좌우 방향에 따른 다음 방향 인덱스 설정
 				if (direction == 'D') {
 					child = (row + 1) * width + col;
 				} else if (direction == 'L') {
@@ -39,15 +42,13 @@ public class Main {
 				} else if (direction == 'U') {
 					child = (row - 1) * width + col;
 				}
-//				System.out.println("parent : " + parent);
-//				System.out.println("child : " + child);
-
+				
+				//부모 자식 연결
 				union(child, parent);
-//				System.out.println(Arrays.toString(parents));
 			}
 		}
 
-		Set<Integer> safeZone = new HashSet<>();
+		Set<Integer> safeZone = new HashSet<>();	//safe zone 의 개수 = 루트 부모의 개수
 
 		for (int i = 0; i < parents.length; i++) {
 			safeZone.add(getParent(parents[i]));
