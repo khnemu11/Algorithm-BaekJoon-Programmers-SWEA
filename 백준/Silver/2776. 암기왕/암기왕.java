f22424073c450d8a610de7c7fffe3165f469712b
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-06-03 16:57:47 +0900

    Update 암기왕.java
    이분탐색 문제 풀이과정 추가

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -9,8 +10,13 @@ import java.util.StringTokenizer;
 /*
  *	풀이 과정
  *	
+ *	1.
  *	두 집합 간 요소가 있는지 확인 -> 해시 셋을 활용
+ *	걸린 시간 : 1652ms
  *
+ *	2.
+ *	두 집합 간 요소가 있는지 확인 -> 이분 탐색을 활용
+ *  걸린 시간 : 1972ms
  * */
 
 public class Main {
@@ -19,7 +25,10 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int T = Integer.parseInt(br.readLine());
-
+		
+		//해시를 활용한 문제 풀이
+		
+		/*
 		for (int tc = 1; tc <= T; tc++) {
 			Set<Integer> note1 = new HashSet<>();
 
@@ -45,6 +54,29 @@ public class Main {
 				}
 			}
 		}
+		*/
+		//이분 탐색을 이용한 문제 풀이
+		
+		for (int tc = 1; tc <= T; tc++) {
+			int note1Length = Integer.parseInt(br.readLine());
+			int note1[] = Arrays.stream(br.readLine().split(" ")).mapToInt(x -> Integer.parseInt(x)).toArray();
+
+			int note2Length = Integer.parseInt(br.readLine());
+			int note2[] = Arrays.stream(br.readLine().split(" ")).mapToInt(x -> Integer.parseInt(x)).toArray();
+			
+			//이분 탐색을 위한 정렬
+			Arrays.sort(note1);
+			
+			//이분 탐색을 통해 note2에 있는 값이 note1에 있는지 확인
+			for (int val : note2) {
+				int result = 0;
+				if (Arrays.binarySearch(note1, val) >= 0) {
+					result = 1;
+				}
+				bw.write(result + "\n");
+			}
+		}
+
 		bw.close();
 	}
 }
