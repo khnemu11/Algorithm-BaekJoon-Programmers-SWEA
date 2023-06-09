d386ed3682f946e594229bf58dd02a0c9c10d4f6
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-06-09 12:02:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.10 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,20 +2,31 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        
+        Map<String, Integer> name = new HashMap<>();
+        
+        for (int i = 0; i < participant.length; i++) {
+            if (name.get(participant[i]) == null) {
+                name.put(participant[i], 1);    
+            } else {
+                int num = name.get(participant[i]) + 1;
+                name.put(participant[i], num);
+            }
+        }
+        
+        for (int i = 0; i < completion.length; i++) {
+            int num = name.get(completion[i]) - 1;
+            name.replace(completion[i], num);
+        }
+        
         String answer = "";
-        HashMap<String,Integer> players = new HashMap<>();
-        
-        for(String person :completion){
-            players.put(person, players.getOrDefault(person,0)+1);     
-        }
-        for(String person :participant){
-            if(players.get(person)==null || players.get(person)==0){
-                answer = person;
+        for (String nm : name.keySet() ) {
+            int num = name.get(nm);
+            if (num != 0) {
+                answer = nm;
                 break;
-            } 
-            players.put(person, players.get(person)-1);
+            }
         }
-        
         
         return answer;
     }
