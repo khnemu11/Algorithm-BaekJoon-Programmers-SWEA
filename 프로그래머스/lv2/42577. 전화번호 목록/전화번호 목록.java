38e1cd2e4a4fb838e4a2c622bcc395c88a3706de
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-06-11 20:06:01 +0900

    [level 2] Title: 전화번호 목록, Time: 1937.73 ms, Memory: 348 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,56 +1,21 @@
 import java.util.*;
 
-/*
-    접두어 표현을 위해 트라이 사용
-    트라이로 내려가면서 만약 중간에 끝이 나거나 끝이 났는대도 다음이 있다면 접두사가 있다
-    
-*/
-
-
-class Solution {
-    static boolean prefix;
-    
+class Solution {  
     public boolean solution(String[] phone_book) {
-        prefix=true;
-        
-        Trie root = new Trie();
-        
+        Set<String> phoneSet = new HashSet<>();
+        Arrays.sort(phone_book,(o1,o2) -> o2.length() - o1.length());
         for(String phone : phone_book){
-            insert(phone,root);
-            
-            if(!prefix){
-                break;        
+            if(phoneSet.contains(phone)){
+                return false;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<phone.length();i++){
+                sb.append(phone.charAt(i));
+                phoneSet.add(sb.toString());
             }
         }
         
-        return prefix;
+        return true;
     }
-    
-    public void insert(String word, Trie trieNode){
-        for(int i=0;i<word.length();i++){
-            Trie next = new Trie();
-            //현재 매칭하는 문자열을 접두사로 쓰이는 경우
-            if(trieNode.isLast){
-                prefix=false;
-            }
-            
-            if(trieNode.child.get(word.charAt(i))==null){
-                trieNode.child.put(word.charAt(i), next);
-            }
-            
-            next = trieNode.child.get(word.charAt(i));
-            
-            trieNode = next;
-        }
-        //다음 문자열도 있어서 현재 문자열이 접두사가 되는 경우
-        if(trieNode.child.size()>0){
-            prefix=false;
-        }
-        
-        trieNode.isLast=true;
-    }
-}
-class Trie{
-    Map<Character,Trie> child = new HashMap<>();
-    boolean isLast=false;
+
 }
