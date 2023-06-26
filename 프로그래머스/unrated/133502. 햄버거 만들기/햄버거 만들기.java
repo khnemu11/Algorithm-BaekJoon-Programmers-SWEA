ae48dfd65c48d55e998ef86855910e6874d752e3
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-06-27 00:17:37 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.06 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,42 +1,51 @@
 import java.util.*;
-
+/*
+아래에서 위로 쌓인다 -> 스택
+*/
 class Solution {
-    Stack<Integer> stack = new Stack<>();
-    int sequence[] = {1,3,2,1};
+    Stack<Integer> stack = new Stack<>();   //재료 순서
+    int sequence[] = {1,3,2,1}; //햄버거 순서
     public int solution(int[] ingredients) {
-        int count = 0;
+        int count = 0;  //완성된 햄버거 개수
         
+        //재료를 하나씩 올려서 햄버거를 만드는 과정
         for(int ingredient : ingredients){
             stack.add(ingredient);
             
-            while(canPack()){
-                for(int i=0;i<4;i++){
-                    stack.pop();
-                }
+            //포장이 가능한만큼 햄버거 제거
+            while(doPack()){
+                //완성된 햄버거 개수 증가
                 count++;
             }
         }
         
         return count;
     }
-    
-    public boolean canPack(){
+    //포장이 가능한 햄버거 있으면 포장하는 메소드
+    public boolean doPack(){
+        //햄버거 재료 수보다 현재 재료수가 적으면 불가능
         if(stack.size() < sequence.length){
             return false;
         }
+        //확인할 햄버거 재료를 임시 저장하는 스택
         Stack <Integer>temp = new Stack<>();
         boolean valid = true;
         
-        for(int i=0;i<4;i++){
+        //위에서 필요한 재료만큼 꺼내서 확인하는 과정
+        for(int i=0;i<sequence.length;i++){
             temp.add(stack.pop());
+            //불가능 하면 더이상 탐색 X
             if(sequence[i] != temp.peek()){
                 valid=false;
                 break;
             }
         }
         
-        while(!temp.isEmpty()){
-            stack.add(temp.pop());
+        //불가능 하면 임시 배열의 값을 스택에 저장
+        if(!valid){
+            while(!temp.isEmpty()){
+                stack.add(temp.pop());
+            }
         }
         
         return valid;
