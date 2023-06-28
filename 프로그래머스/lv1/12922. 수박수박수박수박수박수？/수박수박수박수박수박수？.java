8c411b8633e504e27a92159524d057ff58d84f57
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-06-28 13:17:02 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+    
+        for(int i=0;i<n/2;i++){
+            sb.append("수박");
+        }
+        if(n%2==1){
+            sb.append("수");
+        }
+        
+        return sb.toString();
+    }
+}
