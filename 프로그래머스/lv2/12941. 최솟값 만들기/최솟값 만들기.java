4b3fa211ee2ddd4b8592d22ce3fd8ba8aedf019e
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-07-08 16:44:56 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.16 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,18 +1,23 @@
 import java.util.*;
-
-class Solution
-{
-    public int solution(int []A, int []B)
-    {
-        int answer = 0;
+/*
+    1 2 4
+    4 4 5
+    (4*4) + (2*4) + (1*5)  = 29
+    
+    정렬하고 양 끝을 곱하고 더하는 것이 최소
+*/
+class Solution{
+    public int solution(int []A, int []B){
+        int sum = 0;
+        
         Arrays.sort(A);
         Arrays.sort(B);
         
         for(int i=0;i<A.length;i++){
-            answer += A[i] * B[B.length-i-1];
+            sum += A[i] * B[B.length-i-1];
         }
         
 
-        return answer;
+        return sum;
     }
 }
