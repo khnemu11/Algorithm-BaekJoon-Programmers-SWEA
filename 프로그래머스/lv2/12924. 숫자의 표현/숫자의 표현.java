ff4d00aedfb3a33d828bbd2331edf96d3dc7a895
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-07-09 22:27:31 +0900

    [level 2] Title: 숫자의 표현, Time: 1.58 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,6 +1,28 @@
+/*
+    숫자의 최대 크기 = 10,000
+    => n^2가능
+    
+    결과 :시간초과
+    =>연속된 수의 합이 목표보다 크면 더이상 탐색을 하지 않게 하여 가지치기
+*/
+
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+        
+        for(int l=1;l<=n;l++){
+            int sum =0;
+            for(int r=l;r<=n;r++){
+                sum +=r;
+                if(sum==n){
+                    count++;
+                    break;
+                }else if(sum > n){
+                    break;
+                }
+            }   
+        }
+        
+        return count;
     }
 }
