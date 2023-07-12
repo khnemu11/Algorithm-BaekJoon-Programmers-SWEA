c52e59b40371baba31f51a4d464e0807aee3a3e1
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-07-12 22:32:19 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 135.67 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,10 +1,14 @@
 import java.util.*;
-
+/*
+    연속 수열 = 2배길이의 수열 
+    5:05
+*/
 class Solution {
     public int solution(int[] elements) {
         int [] expendedElements = new int[elements.length*2 - 1];
         Set<Integer> duplicatedSum = new HashSet<>();
         
+        //2배 수열 생성
         for(int i=0;i<expendedElements.length;i++){ 
             expendedElements[i] = elements[i%elements.length];
             duplicatedSum.add(expendedElements[i]);
@@ -13,10 +17,12 @@ class Solution {
         int[] sums  = new int[expendedElements.length];
         sums[0] = expendedElements[0];
         
+        //2배 누적합 생성
         for(int i=1;i<sums.length;i++){
             sums[i] = sums[i-1] + expendedElements[i];
         }
         
+        //2배 구간합 계산
         for(int i=1;i<elements.length;i++){
             for(int j=0;j+i<expendedElements.length;j++){
                 duplicatedSum.add(sums[j+i]-sums[j]+expendedElements[j]);
