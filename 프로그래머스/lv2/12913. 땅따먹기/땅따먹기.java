9d1a3411baef7d0a76daba80ce09ab88f61a0eb3
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-07-14 12:48:34 +0900

    [level 2] Title: 땅따먹기, Time: 18.60 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -1,13 +1,18 @@
 import java.util.*;
-//4개밖에 안되니까 if문으로 처리
+// 현재 최대값 = 이전 최대값 + 현재 열 => DP
+// 시간 복잡도 = 모든 행의 개수 * 각 열당 최대값 연산 * 모든 열의 개수 = 행의 개수 * 열의 개수 ^2
+//             = 100,000 * 4^4 = 6,400,000 
 class Solution {
     int solution(int[][] land) {
         int answer = 0;
         int maxDp[][] = new int[land.length+1][4];
-
+        
+        //현재 행
         for(int i=1;i<maxDp.length;i++){
+            //최대값을 설정할 열
             for(int j=0;j<land[i-1].length;j++){
                 int max = 0;
+                //최대값 탐색
                 for(int k=0;k<land[i-1].length;k++){
                     if(j == k){
                         continue;
@@ -15,7 +20,6 @@ class Solution {
                     max = Math.max(maxDp[i-1][k],max);
                 }
                 maxDp[i][j] = max +land[i-1][j];
-                // System.out.println(Arrays.toString(maxDp[j]));
             }    
         }
       
