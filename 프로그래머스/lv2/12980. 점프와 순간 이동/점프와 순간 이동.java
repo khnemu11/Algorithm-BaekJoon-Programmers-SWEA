0639c3391caeed1294b28563c42c6930792f6ccc
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-07-17 22:35:00 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -1,16 +1,20 @@
 import java.util.*;
-
+/*
+    무조건 *2를 하는것이 이득
+    하지 못하는 경우에 1칸만 움직이면 짝수가 되서 /2가 가능하므로 /2할 때 홀수인 경우만 센다
+    
+    걸린시간 : 5분 32초
+*/
 public class Solution {
     public int solution(int n) {
         int ans = 0;
         
         while(n>0){
-            if(n %2==0){
-                n = n/2;
-            }else{
+            if(n %2!=0){
                 n = n-1;
-                ans++;
+                ans++;                
             }
+            n = n/2;
         }    
     
         return ans;
