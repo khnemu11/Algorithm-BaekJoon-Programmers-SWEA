034f2fb2bcf5cc18f647233254c860db0366d77f
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-08-25 10:04:14 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 43.16 ms, Memory: 89.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution{
+    public int solution(int [][]board){
+        int answer = 0;
+        int sum[][] = new int[board.length+1][board[0].length+1];
+        
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                sum[i+1][j+1] = sum[i+1][j]+sum[i][j+1] - sum[i][j] + board[i][j];
+            }   
+        }          
+        
+        for(int i=1;i<sum.length;i++){
+            for(int j=1;j<sum[0].length;j++){
+                int len = (int)Math.sqrt(answer);
+                
+                while(i+len <= board.length && j+len <= board[0].length){
+                    int cnt = sum[i+len][j+len] - sum[i-1][j+len] 
+                        - sum[i+len][j-1] + sum[i-1][j-1];     
+                    
+                    if(cnt == (len+1)*(len+1)){
+                        answer = Math.max(cnt,answer);
+                    }
+                    len++;
+                }
+            }  
+        }
+
+        return answer;
+    }
+}
