dd539392a1deffc8ebca38013d5ba1da2e3de634
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-09-04 21:34:38 +0900

    [Gold III] Title: 줄 세우기, Time: 532 ms, Memory: 51572 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,66 +1,56 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.valueOf(st.nextToken());
+		int M = Integer.valueOf(st.nextToken());
 
-		int input[] = Arrays.stream(br.readLine().split(" ")).mapToInt(x -> Integer.valueOf(x)).toArray();
-		int nodeSize = input[0];
-		int edgeSize = input[1];
-
+		int parentNums[] = new int[N + 1];
 		ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-		int nodeParentNum[] = new int[nodeSize + 1];
-		for (int i = 0; i <= nodeSize; i++) {
+
+		for (int i = 0; i <= N; i++) {
 			graph.add(new ArrayList<Integer>());
 		}
 
-		for (int i = 0; i < edgeSize; i++) {
-			input = Arrays.stream(br.readLine().split(" ")).mapToInt(x -> Integer.valueOf(x)).toArray();
-			int start = input[0];
-			int end = input[1];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int parent = Integer.valueOf(st.nextToken());
+			int child = Integer.valueOf(st.nextToken());
 
-			graph.get(start).add(end);
-			nodeParentNum[end]++;
+			graph.get(parent).add(child);
+			parentNums[child]++;
 		}
 
-		Queue<Integer> queue = new LinkedList<>();
+		Queue<Integer> q = new LinkedList<>();
 
-		for (int i = 1; i < nodeParentNum.length; i++) {
-			if (nodeParentNum[i] == 0) {
-				queue.add(i);
+		for (int i = 1; i < parentNums.length; i++) {
+			if (parentNums[i] == 0) {
+				q.add(i);
 			}
 		}
 
-		StringBuilder sb = new StringBuilder();
+		StringBuilder resultBuilder = new StringBuilder();
 
-		while (!queue.isEmpty()) {
-			int start = queue.poll();
-			sb.append(start + " ");
+		while (!q.isEmpty()) {
+			int parent = q.poll();
+			resultBuilder.append(parent + " ");
 
-			for (int i = 0; i < graph.get(start).size(); i++) {
-				int end = graph.get(start).get(i);
-				nodeParentNum[end]--;
-
-				if (nodeParentNum[end] == 0) {
-					queue.add(end);
+			for (int child : graph.get(parent)) {
+				parentNums[child]--;
+				if (parentNums[child] == 0) {
+					q.add(child);
 				}
 			}
 		}
 
-		bw.write(sb.deleteCharAt(sb.length() - 1).toString());
-		bw.flush();
-
-		br.close();
-		bw.close();
+		System.out.println(resultBuilder.deleteCharAt(resultBuilder.length() - 1).toString());
 	}
 }
