d15c65f376977af1e329baf44e87693021e168ff
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-10-18 22:31:46 +0900

    [level 3] Title: 길 찾기 게임, Time: 0.43 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,15 +1,18 @@
+//43분 30초
+
 import java.util.*;
 
 class Solution {
     public Node tree;
-    public List<Integer> preOrderList = new ArrayList<>();
-    public List<Integer> postOrderList = new ArrayList<>();
+    public List<Integer> preOrderList = new ArrayList<>();  //전위 순위 리스트
+    public List<Integer> postOrderList = new ArrayList<>(); //후위 순위 리스트
     
     public int[][] solution(int[][] nodeinfo) {
         int[][] answer = new int[2][nodeinfo.length];
         
         List<Node> nodeList = new ArrayList<>();
         
+        //노드를 생성
         for(int i=0;i<nodeinfo.length;i++){
             nodeList.add(new Node(
                 nodeinfo[i][0],
@@ -18,25 +21,25 @@ class Solution {
             ));
         }
         
+        //루트 노드를 찾기위한 정렬
         Collections.sort(nodeList);
+        //루트 노드를 트리 맨 위에 저장
         tree = nodeList.get(0);
-        divideTree(nodeList,tree);
+        makeTree(nodeList,tree);
         
-        // System.out.println(tree);
-        
+        //전위/후위 순회 저장
         setOrder(tree);
         
         for(int i=0;i<answer[0].length;i++){
             answer[0][i] = (int)preOrderList.get(i);
             answer[1][i] = (int)postOrderList.get(i);
         }
-        // System.out.println(preOrderList);       
-        // System.out.println(postOrderList);
         
         return answer;
     }
     
     public void setOrder(Node curr){
+        //전위 순회
         preOrderList.add(curr.seq);
         
         if(curr.left !=null){
@@ -45,18 +48,20 @@ class Solution {
         if(curr.right !=null){
             setOrder(curr.right);
         }   
+        
+        //후위 순회
         postOrderList.add(curr.seq);
     }
-    public void divideTree(List<Node> nodeList, Node parent){
+    //트리를 만드는 메소드
+    public void makeTree(List<Node> nodeList, Node parent){
         if(nodeList.size()<=1){
             return ;
         }
         
-        // System.out.println(parent);
-        
         List<Node> leftList = new ArrayList<>();
         List<Node> rightList = new ArrayList<>();
            
+        //본인 노드를 제외한 나머지 노드를 왼쪽, 오른쪽 구분
         for(int i=1;i<nodeList.size();i++){
             Node curr = nodeList.get(i);
             
@@ -66,22 +71,20 @@ class Solution {
                 rightList.add(curr);
             }
         }
-       
+        //왼쪽 노드가 있으면 왼쪽 트리 생성
         if(leftList.size() > 0){
             Collections.sort(leftList);
             Node left = leftList.get(0);
             parent.left = left;
-            divideTree(leftList,left);
+            makeTree(leftList,left);
         }
+        //오른쪽 노드가 있으면 오른쪽 트리 생성
         if(rightList.size() > 0){
             Collections.sort(rightList);
             Node right = rightList.get(0);
             parent.right = right;
-            divideTree(rightList,right);
+            makeTree(rightList,right);
         }
-        
-       // System.out.println("left : "+leftList);
-       // System.out.println("right : "+rightList);
     }
 }
 
@@ -97,11 +100,7 @@ class Node implements Comparable<Node>{
         this.y = y;
         this.seq = seq;
     }
-    
-    public String toString(){
-        return seq+" : "+x+" , "+y;
-    }
-    
+
     public int compareTo(Node o){
         return o.y - this.y;
     }
