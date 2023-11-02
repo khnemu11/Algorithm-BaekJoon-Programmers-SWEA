7bd41e44f3ab11e6eeca2b38c09e2ac6b25cd768
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2023-11-02 19:31:30 +0900

    [Gold V] Title: 수 고르기, Time: 372 ms, Memory: 27544 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		int N = Integer.parseInt(input[0]);
+		int M = Integer.parseInt(input[1]);
+
+		int[] arr = new int[N];
+
+		for(int i=0;i<arr.length;i++){
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.sort(arr);
+
+		int l=0,r=0;
+		long min_differ = 10_000_000_001L;
+
+		while(r < arr.length){
+			long differ = arr[r] - arr[l];
+			if(differ >= M && min_differ > differ ){
+				min_differ = differ;
+			}
+
+			if(differ >= M && l < r){
+				l++;
+			}else {
+				r++;
+			}
+		}
+
+		System.out.println(min_differ);
+	}
+}
