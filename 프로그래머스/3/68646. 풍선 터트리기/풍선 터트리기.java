1397e08fe0b50f314b93d59b2da72b2eddda0eb8
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2024-01-01 16:07:01 +0900

    [level 3] Title: 풍선 터트리기, Time: 444.44 ms, Memory: 203 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,6 +1,8 @@
 /*
     자기 자신이 이전값과 상관 없이 다음 값들이 자신보다 무조건 크다면 마지막까지 남을 수 있음
     -> 정방향, 역방향으로 증가하는 배열 탐색
+    
+    33분 12초
 */
 
 import java.util.*;
@@ -8,8 +10,8 @@ import java.util.*;
 class Solution {
     public int solution(int[] a) {
         int answer = 0;
+        //가능한 번호가 오른쪽 방향/왼쪽 방향이 같을 수 있으므로 중복 제거
         Set<Integer> possibleBalloons = new HashSet<>();        
-        
         
         //오른쪽 방향
         Stack<Integer> increaseBalloons = new Stack<>();
@@ -21,6 +23,8 @@ class Solution {
             }
             increaseBalloons.push(a[i]);
         }
+        
+        //가능한 풍선 번호 저장
         while(!increaseBalloons.isEmpty()){
             possibleBalloons.add(increaseBalloons.pop());
         }
@@ -36,6 +40,7 @@ class Solution {
             increaseBalloons.push(a[i]);
         }
         
+        //가능한 풍선 번호 저장
         while(!increaseBalloons.isEmpty()){
             possibleBalloons.add(increaseBalloons.pop());
         }
