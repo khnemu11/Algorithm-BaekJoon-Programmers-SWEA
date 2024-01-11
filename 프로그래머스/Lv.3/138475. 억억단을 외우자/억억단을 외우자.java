bfe27449a0853d1e71220e8076c58af522a88708
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2024-01-12 03:50:31 +0900

    [level Lv.3] Title: 억억단을 외우자, Time: 252.64 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/Lv.3/138475. 억억단을 외우자/억억단을 외우자.java
+++ 프로그래머스/Lv.3/138475. 억억단을 외우자/억억단을 외우자.java
@@ -1,24 +1,8 @@
-/*
-1   2   3   4   5   6   7   8
-2   4   6   8   10  12  14  16
-3   6   9   12  15  18  21  24
-4   8   12  16  20  24  28  32
-5   10  15  20  25  30  35  40
-6   12  18  24  30  36  42  48
-7   14  21  28  35  42  49  56
-8   16  24  32  40  48  56  64
-    
-1 2 3 4 5 6 7 8
-1
-1 2
-1 3
-1 2 4
-1 5
-1 2 3 6
-1 7
-1 2 4 8
-*/
 import java.util.*;
+
+/*
+    
+*/
 
 class Solution {
     public int[] solution(int e, int[] starts) {
@@ -27,7 +11,15 @@ class Solution {
         int[] maxCountNum = new int[e+1];
         
         for(int i=1;i<=e;i++){
-            // count[i] = getCount(i);
+            // for(int j=1;j<=Math.sqrt(i);j++){
+            //     if(Math.pow(j,2) == i){
+            //         count[i]++;
+            //     }
+            //     else if(i % j == 0){
+            //         count[i]+=2;
+            //     }
+            // }
+            
             for(int j=i;j<=e;j+=i){
                 count[j]++;
             }
