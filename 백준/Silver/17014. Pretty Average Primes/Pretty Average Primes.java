274ee7929441d8d27f1aac2be1b64cfff37b751e
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2024-07-02 02:28:55 +0900

    [Silver I] Title: Pretty Average Primes, Time: 196 ms, Memory: 24060 KB -BaekjoonHub

--- 백준/Silver/17014. Pretty Average Primes/Pretty Average Primes.java
+++ 백준/Silver/17014. Pretty Average Primes/Pretty Average Primes.java
@@ -12,7 +12,8 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         boolean[] isNotPrime = new boolean[MAX_N+1];
         List<Integer> primeList = new ArrayList<>();
-
+        
+        //에라토스테네스의 체로 소수 판정 및 소수 리스트 생성
         for(int i=2;i<isNotPrime.length;i++){
             if(isNotPrime[i]){
                 continue;
@@ -27,14 +28,18 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        //모든 테스트 케이스를 받아 A와 B 탐색
         for(int testcase=1;testcase<=T;testcase++){
             int N = Integer.parseInt(br.readLine());
 
+            //A와 B가 모두 소수이고 (A*B)/2 = N을 만족하는지 판정
             for(int i=0;primeList.get(i) <= N;i++){
-                int target = N*2 - primeList.get(i);
-
-                if(!isNotPrime[target]){
-                    bw.write(primeList.get(i)+" "+target);
+                int A = primeList.get(i);
+                int B = N*2 - primeList.get(i);
+                
+                //B가 소수이면 주어진 조건을 만족하므로 출력
+                if(!isNotPrime[B]){
+                    bw.write(A+" "+B);
                     bw.newLine();
                     break;
                 }
