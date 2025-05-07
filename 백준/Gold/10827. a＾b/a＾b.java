55694b57a52ac29fc9e8ce12d0a4a6a9e249bb34
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2025-05-07 22:38:11 +0900

    [Gold V] Title: a^b, Time: 116 ms, Memory: 14496 KB -BaekjoonHub

---
+++ 백준/Gold/10827. a＾b/a＾b.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigDecimal a = new BigDecimal(st.nextToken());
+        int  b = Integer.parseInt(st.nextToken());
+
+        BigDecimal result = a.pow(b);
+        System.out.println(result.toPlainString());
+    }
+}
